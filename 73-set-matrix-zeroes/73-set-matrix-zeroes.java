694221ec171981259f754000f01cb1c42055dfec
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-06-17 02:17:49 -0700

    Time: 11 ms (5.08%), Space: 54.3 MB (27.51%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -2,37 +2,51 @@ import java.util.*;
 public class Solution {
     public void setZeroes(int[][] matrix) {
         // int zerodha[][]= new int [matrix.length][matrix[0].length];
-        Queue<Integer> a= new LinkedList<>();
+        // Queue<Integer> a= new LinkedList<>();
+        int[] m = new int[matrix.length];
+        int[] n= new int [matrix[0].length];
         
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if(matrix[i][j]==0){
-                    a.add(i);
-                    a.add(j);
-
-                
+                    m[i]=1;
+                    n[j]=1;
                 }
-
-                
             }
         }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(m[i]==1 || n[j]==1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        // System.out.println(matrix.length);
         
-        while(a.size()!=0){
-            int i=a.poll();
-            int j= a.poll();
-            for (int k = 0;  k< matrix[0].length; k++) {
-                matrix[i][k]=0;
+                    
+                    // matrix[i][j]=-1;
+                    // matrix[0][j]=-1;
+//                     for (int k = 0;  k< matrix[i].length; k++) {
+//                         if(matrix[i][k]!=0){
+//                         matrix[i][k]=-1;}
                 
-            }
-            for (int k = 0;  k< matrix.length; k++) {
-                matrix[k][j]=0;
-                
-            }
-        }
+//                     }
+//                     for (int k = 0;  k< matrix.length; k++) {
+//                         if(matrix[k][j]!=0){
+//                         matrix[k][j]=-1;}
+//                     } 
+                                
+        // for (int i = 0; i < matrix.length; i++) {
+        //     for (int j = 0; j < matrix[0].length; j++) {
+        //         if(matrix[i][j]==-1){
+        //             matrix[i][j]=0;
+        //         }
+        //     }
+        // }
+           
+        
         for (int i=0;i<matrix.length;i++){
         System.out.println(Arrays.toString(matrix[i]));
-        }
-        
-        
+        }        
     }
 }
