7e43137cdd94e042701f300750450bd08fd98cd4
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-06-17 04:19:00 -0700

    Time: 1 ms (95.72%), Space: 44.2 MB (79.89%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -3,24 +3,37 @@ public class Solution {
     public void setZeroes(int[][] matrix) {
         // int zerodha[][]= new int [matrix.length][matrix[0].length];
         // Queue<Integer> a= new LinkedList<>();
-        int[] m = new int[matrix.length];
-        int[] n= new int [matrix[0].length];
-        
+        // int[] m = new int[matrix.length];
+        // int[] n= new int [matrix[0].length];
+        boolean col= true;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][0]==0){
+                col=false;
+            }
+            for (int j = 1; j < matrix[0].length; j++) {
                 if(matrix[i][j]==0){
-                    m[i]=1;
-                    n[j]=1;
+                    matrix[i][0]=matrix[0][j]=0;
                 }
             }
         }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if(m[i]==1 || n[j]==1){
+        for (int i=matrix.length-1;i>=0;i--){
+            for (int j= matrix[0].length-1;j>=1;j--){
+                if (matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
+                
+            }
+            if (col==false){
+        matrix[i][0]=0;    
             }
         }
+        // for (int i = 0; i < matrix.length; i++) {
+        //     for (int j = 0; j < matrix[0].length; j++) {
+        //         if(m[i]==1 || n[j]==1){
+        //             matrix[i][j]=0;
+        //         }
+        //     }
+        // }
         // System.out.println(matrix.length);
         
                     
@@ -43,10 +56,5 @@ public class Solution {
         //         }
         //     }
         // }
-           
-        
-        // for (int i=0;i<matrix.length;i++){
-        // System.out.println(Arrays.toString(matrix[i]));
-        // }        
     }
 }
