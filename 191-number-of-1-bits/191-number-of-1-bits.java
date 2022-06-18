3ec72ec22886bf9adaf3539850c538f3f4bf56cb
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-06-17 22:35:36 -0700

    Time: 1 ms (83.70%), Space: 41.3 MB (39.17%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -0,0 +1,27 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        // System.out.println(n);
+        
+//         Approach1 
+        // convert number to binary, now check for each digit to be one or zero.
+            
+//         Approach2 
+        // right shift the number until n!=0
+        int siu=0;
+        
+        // while (n!=0){
+        for(int i=0;i<32;i++){
+//             checking the last digit is one or zero
+            if((n&1)==1)
+                siu++;
+            
+            n>>=1;
+            if (n==0){
+                break;
+            }
+        }
+    
+        return siu;
+    }
+}
