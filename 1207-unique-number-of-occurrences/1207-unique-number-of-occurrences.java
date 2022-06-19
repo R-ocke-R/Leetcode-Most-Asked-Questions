bcf5339ff014d9fea19969b19979fcaeacaba800
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-06-19 03:04:45 -0700

    Time: 2 ms (92.82%), Space: 42.8 MB (8.39%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,23 +1,18 @@
 class Solution {
-//     public boolean uniqueOccurrences(int[] arr) {
-// //         most basic approach by storing the frequency of each element in HashMap
-//         HashMap<Integer, Integer> map= new HashMap<>();
-//         for(int i=0;i<arr.length;i++){
-//             map.put(arr[i], 1 + map.getOrDefault(arr[0], 0));
-//         }
-//         Set<Integer> set = new HashSet<Integer> (map.values());
-//         if(set.size()==map.size()){
-//             return true;
-//         }
-//         return false;
-    
-//     }
     public boolean uniqueOccurrences(int[] arr) {
-    Map<Integer, Integer> count = new HashMap<>();
-    for (int a : arr)
-        count.put(a, 1 + count.getOrDefault(a, 0));
-    return count.size() == new HashSet<>(count.values()).size();
-}
+//         most basic approach by storing the frequency of each element in HashMap
+        HashMap<Integer, Integer> map= new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i], 1 + map.getOrDefault(arr[i], 0));
+        }
+        Set<Integer> set = new HashSet<Integer> (map.values());
+        if(set.size()==map.size()){
+            return true;
+        }
+        return false;
+    
+    }
+    
 }
 // other approach:
 // make array of 2000 size based on the constraints, now take frequency in the array, 
