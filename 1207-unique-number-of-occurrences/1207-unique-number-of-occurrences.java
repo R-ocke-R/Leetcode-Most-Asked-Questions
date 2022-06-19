fa53597f44628ae9249aad845d9afc767457174b
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-06-19 03:04:11 -0700

    Time: 4 ms (27.97%), Space: 41.5 MB (76.76%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,25 +1,25 @@
 class Solution {
-    public boolean uniqueOccurrences(int[] arr) {
-//         most basic approach
-        HashMap<Integer, Integer> map= new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            if(map.containsKey(arr[i])){
-                int valu=map.remove(arr[i]);
-                valu++;
-                // System.out.println(valu);
-                map.put(arr[i],(valu));
-                // System.out.println(map.get(arr[i]));
-            }
-            else{
-            map.put(arr[i], 1);
-            }
-        }
-        // for (Map.Entry<Integer, Integer> e : map.entrySet()){
-        Set<Integer> set = new HashSet<Integer> (map.values());
-        if(set.size()==map.size()){
-            return true;
-        }
-        return false;
+//     public boolean uniqueOccurrences(int[] arr) {
+// //         most basic approach by storing the frequency of each element in HashMap
+//         HashMap<Integer, Integer> map= new HashMap<>();
+//         for(int i=0;i<arr.length;i++){
+//             map.put(arr[i], 1 + map.getOrDefault(arr[0], 0));
+//         }
+//         Set<Integer> set = new HashSet<Integer> (map.values());
+//         if(set.size()==map.size()){
+//             return true;
+//         }
+//         return false;
     
-    }
+//     }
+    public boolean uniqueOccurrences(int[] arr) {
+    Map<Integer, Integer> count = new HashMap<>();
+    for (int a : arr)
+        count.put(a, 1 + count.getOrDefault(a, 0));
+    return count.size() == new HashSet<>(count.values()).size();
 }
+}
+// other approach:
+// make array of 2000 size based on the constraints, now take frequency in the array, 
+// now sort this array, 
+// traverse the array and find out, if any adjacent element is the same.
