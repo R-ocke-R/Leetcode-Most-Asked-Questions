7309ba41800914e3b4d60861b0f5a934f8526d9f
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-06-18 23:25:50 -0700

    Time: 1 ms (99.92%), Space: 42.6 MB (85.63%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,22 +1,11 @@
 // better approach o(n^2) complexity, constant extra space. 
 class Solution {
     public int singleNumber(int[] arr) {
+        int ans=0;
         for(int i=0;i<arr.length;i++){
-            if(freq(arr, arr[i] )){
-                return arr[i];
+            ans^=arr[i];
             }
-        } return arr[0];
+        return ans;
     }
-    public static boolean freq(int[] arr, int n){
-        int count=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==n){
-            count++;
-            }
-        }
-        if (count==2){
-            return false;
-        }
-        return true;
-}
+
 }
