156546355c4070ac7e8debd7fb734c4a1f54619b
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-06-20 11:11:29 -0700

    Time: 59 ms (5.35%), Space: 68.5 MB (29.28%) - LeetHub

---
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+//         using same approach like question to find all duplicateds in array as this also has the same vital information 
+//         that integers are in the range [1, n]
+        List<Integer> ans= new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            int index=Math.abs(nums[i])-1;
+            if (nums[index]<0) continue;
+            nums[index]=-nums[index];
+        }
+        System.out.println(Arrays.toString(nums));
+        for(int i=0;i<nums.length;i++)
+        {
+            // int index=Math.abs(nums[i])-1;
+            if (nums[i]>0) ans.add(i+1);
+            
+        }
+        return ans;
+        
+    }
+}
