ba403263bf3972bf2dad94d91d614fa60331dbf3
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-06-21 01:01:50 -0700

    Time: 7 ms (79.56%), Space: 70.8 MB (15.62%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
@@ -4,14 +4,12 @@ class Solution {
 //         that integers are in the range [1, n]
         List<Integer> ans= new ArrayList<>();
         for(int i=0;i<nums.length;i++){
-            // int index=Math.abs(nums[i])-1;
-            if (nums[Math.abs(nums[i])-1]>0)
-            nums[Math.abs(nums[i])-1]*=-1;
+            int index=Math.abs(nums[i])-1;
+            if (nums[index]>0)
+            nums[index]*=-1;
         }
-        System.out.println(Arrays.toString(nums));
         for(int i=0;i<nums.length;i++)
         {
-            // int index=Math.abs(nums[i])-1;
             if (nums[i]>0) ans.add(i+1);
             
         }
