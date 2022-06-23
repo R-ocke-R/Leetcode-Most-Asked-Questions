71d8db8d53f86f2e1f37378a44f4b7b41ad2e90c
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-06-23 14:17:53 +0530

    Time: 0 ms (100.00%), Space: 46.6 MB (26.33%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -1,12 +1,31 @@
 class Solution {
-    public int peakIndexInMountainArray(int[] arr) {
-//         ofcourse the O(n) solution is very simple and straight forward
-        int i=0;
-        while (true){
-            if(arr[i++]>arr[i]){
-                break;
+//     public int peakIndexInMountainArray(int[] arr) {
+// //         ofcourse the O(n) solution is very simple and straight forward
+//         int i=0;
+//         while (true){
+//             if(arr[i++]>arr[i]){
+//                 break;
+//             }
+//         }
+//         return --i;
+//     }
+    public int peakIndexInMountainArray(int[] arr){
+//         now attempting to implement a logarithmic solution, by modifying BS
+        int s=0;
+        int e=arr.length-1;
+        while(s<=e){
+            int mid=s+((e-s)/2);
+            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]){
+                return mid;
             }
+            else if(arr[mid-1]<arr[mid]){
+                s=mid+1;
+            }
+            else{
+                e=mid;
+            }
+            
         }
-        return --i;
+        return -1;
     }
 }
