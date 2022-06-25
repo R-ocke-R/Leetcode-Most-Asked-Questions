449502bce1c236213d9a56fc5fb64e1ae7981ed4
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-06-25 20:10:51 +0530

    Time: 1 ms (35.74%), Space: 48 MB (6.23%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,50 +1,58 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-
-        int [] buffer=BSearch(nums, target, 0, nums.length-1);
-        if(buffer[0]==-1){
-            return new int[]{-1, -1};
-        }
-        int first=buffer[0];
-        int last=buffer[0];
-        int end=buffer[2];
-
+    public int[] BinarySearch(int s, int e, int[] nums, int target){
+        int mid=s+((e-s)/2);
         
-        while (true){
-            buffer=BSearch(nums, target, buffer[1], first-1);
-
-            if(buffer[0]==-1){
-                break;
+        while(s<=e){
+            if(nums[mid]==target){
+                return new int[]{mid, s, e};
             }
-            first=buffer[0];
-            // buffer[0]=buffer[1];           
-        }
-        
-        buffer[2]=end;
-        
-        while(true){
-            buffer=BSearch(nums, target, last+1, buffer[2]);
-            if(buffer[2]==-1){
-                break;
-            }
-            last=buffer[0];
-        }
-        return new int[]{first, last};
-    }
-    
-    public static int[] BSearch(int[] arr, int ele, int start, int end){        
-        while(start<=end){
-            int mid=start+((end-start)/2); 
-            if (arr[mid]==ele){
-                return (new int[]{mid, start, end});
-            }
-            else if(arr[mid]>ele){
-                end=mid-1;
+            else if(nums[mid]>target){
+                e=mid-1;
             }
             else{
-                start=mid+1;
+                s=mid+1;
+                
             }
+            mid=s+((e-s)/2);
+    
+        }return new int[]{-1, -1, -1}; 
+    }
+    public int[] searchRange(int[] nums, int target) {
+        if (nums.length==0){
+            return new int[]{-1, -1};
         }
-        return new int[]{-1, -1, -1};
+        
+        int s=0;
+        int e=nums.length-1;
+        int [] dummy=BinarySearch(0 ,e, nums, target);
+        if(dummy[0]==-1){
+            return new int[]{-1, -1};
+        }
+        int first=dummy[0];
+        int last=dummy[0];
+        int end=dummy[2];
+//         for first
+        while(true){
+            dummy=BinarySearch(dummy[1], first-1, nums, target);
+            if(dummy[0]==-1){
+                break;
+            }
+            first=dummy[0];            
+        }
+        // System.out.println(first);
+        // return new int[]{-2, -1};
+        dummy[2]=end;
+        while(true){
+            dummy=BinarySearch(last+1, dummy[2], nums, target);
+            if(dummy[0]==-1){
+                break;
+            }
+            // first=dummy[0];
+            last=dummy[0];
+        }
+        // System.out.println(last);
+        return new int[]{first, last};
+        // return new int[]{-2, -1};
+    
     }
 }
