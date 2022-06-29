c2758b99d388a2a0d6e0c7b56bf70fde243f8546
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-06-29 12:37:28 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (32.30%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,42 +1,58 @@
 class Solution {
-    public void rotate(int[] arr, int k){
-        k=k%arr.length;
-        rr(arr, 0, arr.length-1);
-        rr(arr, 0, k-1);
-        rr(arr, k, arr.length-1);
+//     public void rotate(int[] arr, int k){
+//         k=k%arr.length;
+//         rr(arr, 0, arr.length-1);
+//         rr(arr, 0, k-1);
+//         rr(arr, k, arr.length-1);
             
-    }
-    public void rr(int[] arr, int s, int e){
-        while(s<e){
-            int temp=arr[s];
-            arr[s]=arr[e];
-            arr[e]=temp;
-            s++;
-            e--;
+//     }
+//     public void rr(int[] arr, int s, int e){
+//         while(s<e){
+//             int temp=arr[s];
+//             arr[s]=arr[e];
+//             arr[e]=temp;
+//             s++;
+//             e--;
             
-        }
-    }
+//         }
+//     }
     
-    public boolean check(int[] nums) {
-        int k=0;
-        boolean ans=true;
-        while(k<nums.length){
-            rotate(nums, 1);
-            k++;
-            ans=true;
-            // System.out.println(Arrays.toString(nums));
-            for(int i=0;i<nums.length-1;i++){
-                if(nums[i]>nums[i+1]) {
-                    // System.out.println(nums[i] +" "+ nums[i+1]);
-                    ans=false;
-                    break;
-                }
-            }
-            // System.out.println(ans);
-            if(ans==true){
-                return true;
-            }
+//     public boolean check(int[] nums) {
+//         int k=0;
+//         boolean ans=true;
+//         while(k<nums.length){
+//             rotate(nums, 1);
+//             k++;
+//             ans=true;
+//             // System.out.println(Arrays.toString(nums));
+//             for(int i=0;i<nums.length-1;i++){
+//                 if(nums[i]>nums[i+1]) {
+//                     // System.out.println(nums[i] +" "+ nums[i+1]);
+//                     ans=false;
+//                     break;
+//                 }
+//             }
+//             System.out.println(ans);
+//             if(ans==true){
+//                 return true;
+//             }
             
+//         }
+//         return false;
+//     }
+    public boolean check(int[] nums){
+        int count=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]<=nums[i+1]){
+                continue;
+            }
+            else{
+                count++;
+            }
+        }
+        if(nums[0]<nums[nums.length-1]) count++;
+        if(count==0 || count ==1){
+            return true;
         }
         return false;
     }
