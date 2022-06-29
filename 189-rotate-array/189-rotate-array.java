545f6619fc8cc9a584af64695ac9315ebfe37006
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-06-29 12:00:51 +0530

    Time: 1 ms (85.12%), Space: 64.7 MB (21.69%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,49 +1,32 @@
 import java.util.*;
 class Solution {
-//     public void rotate(int[] nums, int k) {
-// //         first : the places are decided by k not by rotating.
-        
-//         int n=nums.length;
-//         int j=0;
-//         // System.out.println(k%n);
-//         while(j<(k%n)){
-//             int temp=nums[n-1];
-//             for(int i=n-1;i>0;i--){
-//                 nums[i]=nums[i-1];
-//             }
-//             nums[0]=temp;
-//             j++;
-//         }
-//         // System.out.println(Arrays.toString(nums));
-//     }
-//     public void rotate(int[] nums, int k){
-// //         second using k to determine indexes, O(n) solution for both space+time
-//         int[] arr= new int [nums.length];
-//         k=k%arr.length;
-//         int j=0;
-//         for(int i=k+1;i<nums.length;i++){
-//             arr[j++]=nums[i];
-//         }
-        
-//         for(int i=0;i<=k;i++){
-//             arr[j++]=nums[i];
-//         // }System.out.println(Arrays.toString(arr));
-//         }
-//         for(int i=0;i<arr.length;i++){
-//             nums[i]=arr[i];
-//         }
-
-        
-//         // return arr;
-//     }
-    public void rotate (int [] arr, int k){
-        int[] temp=new int[arr.length];
-        for(int i=0;i<arr.length;i++){
-            temp[(i+k)%arr.length]=arr[i];
+//     Approach 1: Solving with formuale using O(N) time and space complexity,
+    
+    // public void rotate (int [] arr, int k){
+    //     int[] temp=new int[arr.length];
+    //     for(int i=0;i<arr.length;i++){
+    //         temp[(i+k)%arr.length]=arr[i];
+    //     }
+    //     for(int i=0;i<arr.length;i++){
+    //         arr[i]=temp[i];
+    //     }
+    // }    
+//     Approach 2: Solving with a O(n) time and O(1) space complexity,
+    public void rotate(int[] arr, int k){
+        k=k%arr.length;
+        rr(arr, 0, arr.length-1);
+        rr(arr, 0, k-1);
+        rr(arr, k, arr.length-1);
+            
+    }
+    public void rr(int[] arr, int s, int e){
+        while(s<e){
+            int temp=arr[s];
+            arr[s]=arr[e];
+            arr[e]=temp;
+            s++;
+            e--;
+            
         }
-        for(int i=0;i<arr.length;i++){
-            arr[i]=temp[i];
-        }
-        
-    }    
+    }
 }
