9427b3a457da543c296fe8d44ac9d40588a48931
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-07-01 17:10:18 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (48.04%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,34 +1,21 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=0;
-        int j=0;
-        int[] nums3=new int[m];
-//         one approach is to have O(n+m) space or rather O(n) space
-//         other involve
-        while(i<m){
-            nums3[i]=nums1[i];
-            i++;
+        int i=m-1;
+        int j=n-1;
+        //Approach 2: using no extra space, Trick: start filling nums1 in decreasing order from last
+        int k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j])
+            nums1[k--]=nums1[i--];
+            else nums1[k--]=nums2[j--];
             
         }
-        i=0;
-        int k=0;
-        while(i<n && j<m){
-            if (nums2[i]<nums3[j]){
-                nums1[k++]=nums2[i++];
-            }
-            else{
-                nums1[k++]=nums3[j++];
-            }
-        }
-        while(i<n){
-            nums1[k++]=nums2[i++];
-
-        }
-        while(j<m){
-            nums1[k++]=nums3[j++];
-
-        }
+        // if nums1 elements are left after this with condition while(i>=0)
+        // they are at there right place. cos of sorted nums1
+        //but have to consider it for num2
         
-        
+        while(j>=0){
+            nums1[k--]=nums2[j--];
+        }        
     }
 }
