2bdaed6c565699b62bcd91bd599f724815242300
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-07-01 19:21:01 +0530

    Time: 2 ms (79.01%), Space: 55.1 MB (27.45%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -2,25 +2,16 @@ class Solution {
     public static void approach3(int[] arr){
         //optimisation of time complexity.
         
-        int i=0;//for zero.
-        while(i<arr.length){
-            if(arr[i]==0){
-                //find next nonzero to swap with.
-                for (int j = i; j < arr.length; j++) {
-                    if(arr[j]!=0){
-                        arr[i]=arr[j];
-                        arr[j]=0;
-                        break;
-                    }
-                    continue;
-                }
-                if(arr[i]==0){
-                    break;//thus no more zero's
-                }
+        int nonzero=0;
+        for (int j = 0; j < arr.length; j++) {
+            if(arr[j]!=0){
+                int temp=arr[nonzero];
+                arr[nonzero]=arr[j];
+                arr[j]=temp;
+                nonzero++;
+
             }
-            else i++;
         }
-        // System.out.println(Arrays.toString(arr));
     }
     public void moveZeroes(int[] nums) {
         approach3(nums);
