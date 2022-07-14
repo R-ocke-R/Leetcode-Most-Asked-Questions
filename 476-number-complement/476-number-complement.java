52f1fdbab1ed794d1730ff9d95ae3d49407e2b8f
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-07-14 14:45:31 +0530

    Time: 1 ms (52.77%), Space: 41.3 MB (29.83%) - LeetHub

--- 476-number-complement/476-number-complement.java
+++ 476-number-complement/476-number-complement.java
@@ -1,15 +1,28 @@
 class Solution {
     public int findComplement(int num) {
-        if(num==0){
-            return 1;
+        // if(num==0){
+        //     return 1;
+        // }
+        // int mask=0;
+        // int m=num;
+        // while(m!=0){
+        //     mask=(mask <<1) | 1;
+        //     m>>=1;
+        // }
+        // return mask&(~num);
+        // int ans;.
+        if(num==1)return 0;
+        int set=31;
+        for(int i=31;i>=1;i--){
+            if((num & (1<<i))==1<<i){
+                set=i;
+                // System.out.println(i);
+                break;
+            }
         }
-        int mask=0;
-        int m=num;
-        while(m!=0){
-            mask=(mask <<1) | 1;
-            m>>=1;
-        }
-        return mask&(~num);
-        
+        for(int i=0;i<=set;i++){
+            num=num^(1<<i);
+            
+        }return num;
     }
 }
