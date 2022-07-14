4d4f8a8ed71df63c6678c96a2339245a451b6787
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-07-14 17:33:20 +0530

    Time: 2 ms (68.06%), Space: 41.7 MB (19.26%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,39 +1,26 @@
 class Solution {
-    // public static boolean recur(int n){
-        // if(n==0){
-            // return false;
-            
-        // }
-        // else if (n==1){
-            // return true;
-            
-        // }
-        // return recur(n/2);
-    // }
     public boolean isPowerOfTwo(int n) {
-        // return recur(n);
-        // if(n&1==0){
-        //     return false;
-        // }
-        // else{
-        //     return true;
-        // }
-        // // return (n&1)
+//         int count=0;
+//         for(int i=0;i<32;i++){
+//             if((n&(1<<i))==(1<<i)){
+//                 count++;
+//                 if(count==2){
+//                     return false;
+//                 }
+//             }
+            
+//         }
+//         if((count==1 && n!=0) || count==0)return false;
+//         return true;
+        if(n<0) return false;
         int count=0;
         while(n!=0){
-            int bit=(n&1);
-            
-            if (bit==1){
-                n>>=1;
-                count++;
-                break;
-            }
-            n>>=1;
-            
+            n=n&(n-1);
+            count++;
         }
-        if((count==1 && n!=0)|| count==0){
-            return false;
-        }
+        // System.out.println(count);
+        if (count==1)
         return true;
+        return false;
     }
 }
