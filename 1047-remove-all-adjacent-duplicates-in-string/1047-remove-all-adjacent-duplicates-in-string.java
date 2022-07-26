e830ae7e734e1adfc80ac89b18a55183a8e11e92
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-07-26 21:30:14 +0530

    Time: 14 ms (85.03%), Space: 49.4 MB (67.11%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,25 +1,39 @@
 class Solution {
-    public String toStr(Stack<Character> stack){
-        StringBuilder st=new StringBuilder();
-        int ss=stack.size();
-        for(int i=0;i<ss;i++){st.append(stack.pop());}
-        st.reverse();
-        return st.toString();
+//     public String toStr(Stack<Character> stack){
+//         StringBuilder st=new StringBuilder();
+//         int ss=stack.size();
+//         for(int i=0;i<ss;i++){st.append(stack.pop());}
+//         st.reverse();
+//         return st.toString();
         
-    }
-    public String removeDuplicates(String s) {
-        Stack<Character> stack=new Stack<>();
-        int i=0;
-        while(i<s.length()){
-            //check if stack is non empty then peek element, if its equal to charAt(i) pop it and i++; else push.
-            if(stack.size()!=0 && stack.peek()==s.charAt(i)){
-                stack.pop();i++;
-                continue;                
+//     }
+//     public String removeDuplicates(String s) {
+//         Stack<Character> stack=new Stack<>();
+//         int i=0;
+//         while(i<s.length()){
+//             //check if stack is non empty then peek element, if its equal to charAt(i) pop it and i++; else push.
+//             if(stack.size()!=0 && stack.peek()==s.charAt(i)){
+//                 stack.pop();i++;
+//                 continue;                
+//             }
+//             stack.push(s.charAt(i));
+//             i++;
+//         }
+        
+//         return toStr(stack);
+//     }
+    public String removeDuplicates(String s){
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(sb.length()>0){
+                if(sb.charAt(sb.length()-1)==s.charAt(i)){
+                sb.setLength(sb.length()-1);
+                    continue;
+                }
             }
-            stack.push(s.charAt(i));
-            i++;
-        }
-        
-        return toStr(stack);
+            
+            sb.append(s.charAt(i));
+            
+        }return sb.toString();
     }
 }
