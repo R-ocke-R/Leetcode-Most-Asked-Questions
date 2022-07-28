630ca25169970d9592344b6f6862af8b7fddc26e
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-07-28 17:12:10 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (98.32%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,9 +1,18 @@
 class Solution {
     public int fib(int n) {
 
-        if (n==0) return 0;
-        if (n==1) return 1;
-        return fib(n-1)+fib(n-2);
-    
+        if(n==1) return 1;
+
+        int a=0;
+        int b=1;
+        int c=0;
+
+        for (int i = 2; i <= n; i++) {
+            c=a+b;
+            //now c is the new value
+            a=b;
+            b=c;
+        }    
+        return c;
     }
 }
