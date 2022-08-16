368a4a5abf747eb563ac6ed3e191fce7a0dc5fd1
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-08-16 22:50:35 +0530

    Time: 15 ms (51.05%), Space: 44.6 MB (54.65%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,13 +1,22 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0) return false;
+        // if(x<0) return false;
+        // int reverse=0;
+        // int copy=x;
+        // while(copy!=0){
+        //     reverse=(reverse*10)+(copy%10);   
+        //     copy/=10;
+        // }
+        // if(x==reverse) return true;
+        // return false;
+        if(x<0|| x%10==0 && x!=0) return false;
+        // to avoid integer overflow, we will only revert half of the number, somewhat like a two pointer approach
         int reverse=0;
-        int copy=x;
-        while(copy!=0){
-            reverse=(reverse*10)+(copy%10);   
-            copy/=10;
+        while(reverse<x){
+            reverse=reverse*10 + x%10;
+            x/=10;
         }
-        if(x==reverse) return true;
+        if(x==reverse || reverse/10==x) return true;
         return false;
     }
 }
