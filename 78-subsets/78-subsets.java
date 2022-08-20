4ffd8ab75ec58296bc8875004a6983b82375f651
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-08-20 23:50:54 +0530

    Time: 1 ms (89.64%), Space: 43.6 MB (26.12%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,14 +1,14 @@
 class Solution {
-    public void answer(int[] nums, int index, List<Integer> pow, List<List<Integer>> ans){
+    
+    public void answer(int[] nums, int index, List<Integer> pow, List<List<Integer>> ans)
+    
+    {
         if(index==nums.length){
             ans.add(new ArrayList<Integer>(pow));
             return;
         }
         
-        
-        
         answer(nums, index+1, pow, ans);
-        
         pow.add(nums[index]);
         answer(nums, index+1, pow, ans);
         pow.remove(pow.size()-1);
@@ -16,11 +16,12 @@ class Solution {
         return;
     }
     
+    public List<List<Integer>> subsets(int[] nums) 
     
-    public List<List<Integer>> subsets(int[] nums) {
+    {
         List<List<Integer>> list = new ArrayList<>();
         answer(nums, 0, new ArrayList<Integer>(), list);
         return list;
-        
     }
+    
 }
