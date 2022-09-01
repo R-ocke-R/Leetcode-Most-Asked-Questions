39f9ac7a1cbf575330fa956aee574fe08e309fd6
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-09-01 17:07:45 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (98.27%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -25,23 +25,42 @@ class Solution {
     //     head=front;
     //     return head;
     // }
-    public ListNode reverseList(ListNode head) {
-        if(head==null || head.next==null ) return head;
-        ListNode temp= head;
-        ListNode prev=null;
-        ListNode front=head.next;
-        while(front != null){
+//     public ListNode reverseList(ListNode head) {
+//         if(head==null || head.next==null ) return head;
+//         ListNode temp= head;
+//         ListNode prev=null;
+//         ListNode front=head.next;
+//         while(front != null){
             
-            temp.next=prev;
+//             temp.next=prev;
             
-            prev=temp;
-            temp=front;
-            front=front.next;
+//             prev=temp;
+//             temp=front;
+//             front=front.next;
             
-        }
-        temp.next=prev;
-        head=temp;
-        return head;
+//         }
+//         temp.next=prev;
+//         // head=temp;
+//         // return head;
+//         return temp;
+//     }
+    
+    // moving from iterative to recursive approaches
+    public ListNode reverseList(ListNode head){
+        
+        //base cases
+        if(head==null|| head.next==null) return head;
+        // now we know that reverseList function is supposed to return the reverse of the list and pointer to its head
+        ListNode chotaHead=reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        
+        
+        
+        return chotaHead;
     }
+    
+    
+    
     
 }
