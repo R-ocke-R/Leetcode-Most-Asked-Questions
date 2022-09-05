84a63fdfc58e27d98d97224f1662c523b0b58245
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-09-05 23:31:55 +0530

    Time: 1 ms (64.78%), Space: 42.3 MB (50.65%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -2,34 +2,28 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
-        // System.out.println(m+" "+ n);
+        
         int[][] path=new int[m][n];
         
         if(obstacleGrid[0][0]==1) return 0;
+        
         for(int i=0;i<n;i++){
             if(obstacleGrid[0][i]==1) break;
             path[0][i]=1;
         }
+        
         for(int i=0;i<m;i++){
             if(obstacleGrid[i][0]==1) break;
             path[i][0]=1;
         }
-        // for(int i=0;i<m;i++){
-        //     if(obstacleGrid[0][i]==1) break;
-        //     path[0][i]==1;
-        // }
+        
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                if(obstacleGrid[i][j]==0){
-                        path[i][j]=path[i-1][j]+path[i][j-1];    
-                }
-                else{
-                    path[i][j]=0;
-                }
-                
+                if(obstacleGrid[i][j]==0) path[i][j]=path[i-1][j]+path[i][j-1];    
+                else path[i][j]=0;                
             }
-            
         }
+        
         return path[m-1][n-1];
     }
 }
