ab64ac7bdfec19f3babd1ba005706e01e9200bcb
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-09-06 16:26:53 +0530

    Time: 2 ms (30.05%), Space: 43.7 MB (40.57%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,28 +1,39 @@
 class Solution {
     
-    public int recursive(int[] cost, int n, int[] dp){
+//     public int recursive(int[] cost, int n, int[] dp){
         
-        if(n<0) return 0;
-        if(n==0) return cost[0];
+//         if(n<0) return 0;
+//         if(n==0) return cost[0];
         
-        if(dp[n]!=0) return dp[n];
+//         if(dp[n]!=0) return dp[n];
         
-        int low=recursive(cost, n-1, dp);
-        int high=recursive(cost, n-2, dp);
+//         int low=recursive(cost, n-1, dp);
+//         int high=recursive(cost, n-2, dp);
         
-        if(n==cost.length) return Math.min(low, high);
+//         if(n==cost.length) return Math.min(low, high);
         
-        dp[n]=cost[n]+Math.min(low, high);
+//         dp[n]=cost[n]+Math.min(low, high);
         
-        return dp[n];
+//         return dp[n];
         
-    }
+//     }
     
     public int minCostClimbingStairs(int[] cost) {
 
         int n=cost.length;
-        int[] dp= new int[n+1];        
-        return recursive(cost, n, dp);
+        // int[] dp= new int[n+1];        
+        // return recursive(cost, n, dp);
+        int[] dp=new int[n];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<n;i++){
+            // System.out.println(cost[i]+(Math.min(dp[i-1], dp[i-2])));
+            dp[i]=cost[i]+(Math.min(dp[i-1], dp[i-2]));
+        }
+        return Math.min(dp[n-1], dp[n-2]);
+        
         
     }
+    
+
 }
