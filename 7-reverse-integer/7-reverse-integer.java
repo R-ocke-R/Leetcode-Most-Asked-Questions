f9be531dae4083bf5e3c9782fde2f3b63bef9a46
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-09-06 08:44:15 +0530

    Time: 2 ms (77.66%), Space: 40.9 MB (79.26%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,16 +1,28 @@
 class Solution {
-    public int reverse(int x) {
+//     public int reverse(int x) {
+//         int sum=0;
+//         while(x!=0){
+//             if (sum>Integer.MAX_VALUE/10 || sum< Integer.MIN_VALUE/10){ 
+//                 return 0;
+//             }
+//             sum*=10;
+//             sum+=x%10;
+//             x/=10;
+        
+//         }
+        
+//         return sum;
+//     }
+    public int reverse(int x){
         int sum=0;
         while(x!=0){
-            if (sum>Integer.MAX_VALUE/10 || sum< Integer.MIN_VALUE/10){ 
+            if(sum > Integer.MAX_VALUE/10 || sum < Integer.MIN_VALUE/10){
                 return 0;
             }
             sum*=10;
             sum+=x%10;
             x/=10;
-        
         }
-        
         return sum;
     }
 }
