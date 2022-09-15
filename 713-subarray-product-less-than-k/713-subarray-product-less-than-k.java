50e4d5437b8ef233c31f39fa646f52613b1c5694
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-09-15 09:27:08 +0530

    Time: 1740 ms (5.02%), Space: 72.4 MB (36.43%) - LeetHub

---
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+    
+//     this approach wasn't for contingous subarray, and its not even necessary right now.
+//     public void subarrayRecursive(int[] nums, int index){
+//         if(index==nums.length){
+//             //print the subarray formed.
+//         }
+//         for(int i=index, i<nums.length;i++){
+            
+//         }
+//     }
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        //another question on sliding window technique, quite important for understanding all questions around subarrays and stuff.
+        int l=nums.length;
+        if(l<=1) return 0;
+    
+        int count=0;
+        for(int i=0;i<l;i++){
+            int prod=nums[i];
+            if(prod<k) count++;
+            for(int j=i+1;j<l && prod<k;j++){
+                prod*=nums[j];
+                if(prod<k) count++;
+            }
+        }
+        return count;
+    }
+}
