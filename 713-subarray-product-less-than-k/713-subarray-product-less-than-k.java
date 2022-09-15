e350690b2e5be624aa885d7e5c7b259d79fbf638
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-09-15 09:37:33 +0530

    Time: 8 ms (50.76%), Space: 67 MB (79.44%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
@@ -10,19 +10,33 @@ class Solution {
             
 //         }
 //     }
-    public int numSubarrayProductLessThanK(int[] nums, int k) {
-        //another question on sliding window technique, quite important for understanding all questions around subarrays and stuff.
-        int l=nums.length;
-        if(l<=1) return 0;
+//     public int numSubarrayProductLessThanK(int[] nums, int k) {
+//         //another question on sliding window technique, quite important for understanding all questions around subarrays and stuff.
+//         int l=nums.length;
+//         if(l<=1) return 0;
     
+//         int count=0;
+//         for(int i=0;i<l;i++){
+//             int prod=nums[i];
+//             if(prod<k) count++;
+//             for(int j=i+1;j<l && prod<k;j++){
+//                 prod*=nums[j];
+//                 if(prod<k) count++;
+//             }
+//         }
+//         return count;
+//     }
+    public int numSubarrayProductLessThanK(int[] nums, int k){
+        if(k<=1) return 0;
         int count=0;
-        for(int i=0;i<l;i++){
-            int prod=nums[i];
-            if(prod<k) count++;
-            for(int j=i+1;j<l && prod<k;j++){
-                prod*=nums[j];
-                if(prod<k) count++;
-            }
+        int left=0;
+        int prod=1;
+        for(int r=0;r<nums.length;r++){
+            prod*=nums[r];
+            while(prod>=k){
+                prod/=nums[left++];
+                           }
+            count+=r-left+1;
         }
         return count;
     }
