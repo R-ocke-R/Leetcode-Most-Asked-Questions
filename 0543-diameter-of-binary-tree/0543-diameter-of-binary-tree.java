cdc38aec815a4f02cad656246722fcb88ff3a76e
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-10-27 23:40:20 +0530

    Time: 2 ms (20.69%), Space: 43.5 MB (28.59%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,19 +14,36 @@
  * }
  */
 class Solution {
-    public int height(TreeNode root){
-        if (root==null) return 0;
-        return 1+Math.max(height(root.left), height(root.right));
+    class pairr{
+        int hei;
+        int dia;
+        pairr(int diam, int heig){
+            this.hei=heig;
+            this.dia=diam;
+        }
     }
-    public int diameter(TreeNode root) {
-        if(root==null) return 0;
-        int c1=diameter(root.left);
-        int c2=diameter(root.right);
-        int c3= height(root.left) + height(root.right)+1;
-        return Math.max(c1, Math.max(c2, c3));
-    }
+    // public int height(TreeNode root){
+    //     if (root==null) return 0;
+    //     return 1+Math.max(height(root.left), height(root.right));
+    // }
+    // public int diameter(TreeNode root) {
+    //     if(root==null) return 0;
+    //     int c1=diameter(root.left);
+    //     int c2=diameter(root.right);
+    //     int c3= height(root.left) + height(root.right)+1;
+    //     return Math.max(c1, Math.max(c2, c3));
+    // }
     public int diameterOfBinaryTree(TreeNode root) {
-        return diameter(root)-1;
+        // return diameter(root)-1; 
+        // O(n^2) approach
+        return diamter(root).dia-1;
         
+    }
+    public pairr diamter(TreeNode root){
+        if(root==null) return new pairr(0,0);
+        pairr one=diamter(root.left);
+        pairr two=diamter(root.right);
+        pairr ans=new pairr(Math.max(one.hei+two.hei+1, Math.max(one.dia, two.dia)), Math.max(one.hei, two.hei)+1);
+        return ans;
     }
 }
