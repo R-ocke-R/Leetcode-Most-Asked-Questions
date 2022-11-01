e6e23c1b9171c432a014647e0f7d0f9a14b7f508
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-11-01 13:08:45 +0530

    Time: 6 ms (12.42%), Space: 43.2 MB (7.71%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class pair{
+        TreeNode tnode;
+        int level;
+        pair(TreeNode temp, int line){
+            tnode=temp;
+            level=line;
+        }
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        
+        List<Integer> ans=new ArrayList<>(); // to store answer
+        if(root==null) return ans;
+        
+        Queue<pair> queue= new LinkedList<>(); // for level order traversal
+        queue.add(new pair(root, 0));
+        
+        Map<Integer, Integer> map=new TreeMap<>(); // to store the mapping with the line. and treemap to get sorted order
+        while(!queue.isEmpty()){
+            pair temp=queue.remove();
+            
+            map.put(temp.level, temp.tnode.val);
+            
+            // else we don't add, as a value exist for this line.
+            if(temp.tnode.left!=null) queue.add(new pair(temp.tnode.left, temp.level+1));
+            if(temp.tnode.right!=null) queue.add(new pair(temp.tnode.right, temp.level+1));
+            
+        }
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()){
+            ans.add(entry.getValue());
+        }
+        return ans;
+    }
+}
