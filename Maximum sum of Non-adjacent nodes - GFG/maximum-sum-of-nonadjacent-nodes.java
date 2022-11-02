cd24dda7abb43b264ebc1dea06c7e006d5695554
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-11-02 15:52:37 +0530

    Added solution - LeetHub

---
+++ Maximum sum of Non-adjacent nodes - GFG/maximum-sum-of-nonadjacent-nodes.java
@@ -0,0 +1,151 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node left, right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class MaxSum
+{
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution ob = new Solution();
+			    int ans = ob.getMaxSum(root);
+			    System.out.println(ans);
+                t--;
+	        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*class Node
+{
+    int data;
+    Node left, right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}*/
+
+class Solution
+{
+    static class pair{
+        int first;
+        int second;
+        
+        pair(int first, int second){
+            this.first=first;
+            this.second=second;
+        }
+    }
+    static pair solve(Node root){
+        if(root==null) return new pair(0, 0);
+        
+        pair left=solve(root.left);
+        pair right=solve(root.right);
+        
+        pair res= new pair(0, 0);
+        // res signifies the maximum value including the root data.
+        // once I have taken root.data I can't take it for its children 
+        // coz in left and right's first they could have calculated their
+        // root.data and that would be adjacent here.
+        res.first=root.data + left.second + right.second;
+        res.second=Math.max(right.first, right.second)+ Math.max(left.first, left.second);
+        return res;
+        
+    }
+    static int getMaxSum(Node root)
+    {
+        pair ans=solve(root);
+        return Math.max(ans.first, ans.second);
+    }
+}
