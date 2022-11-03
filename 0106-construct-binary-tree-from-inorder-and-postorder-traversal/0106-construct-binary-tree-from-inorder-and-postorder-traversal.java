0f74dccae3650e6a9701adabe4737bbf24876af3
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-11-03 16:23:34 +0530

    Time: 5 ms (31.78%), Space: 44.5 MB (36.89%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int postIndex;
+    public TreeNode builder(int[] inorder, int[] postorder, int inorderStart, int inorderEnd, int length, Map<Integer, Integer> mapping){
+        
+        if(postIndex<0 || inorderStart>inorderEnd) return null;
+        
+        int element=postorder[postIndex--];
+        int rootInInorder=mapping.get(element);
+        
+        TreeNode temp= new TreeNode(element);
+        temp.right=builder(inorder, postorder, rootInInorder+1, inorderEnd, length, mapping);
+        temp.left=builder(inorder, postorder, inorderStart, rootInInorder-1, length, mapping);
+
+        
+        return temp;       
+        
+    }
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        int n=postorder.length;
+        postIndex=n-1;
+        Map<Integer, Integer> mapping= new HashMap<>();
+        for(int i=0;i<n;i++){
+            mapping.put(inorder[i], i);
+        }
+        return builder(inorder, postorder, 0, n-1, n, mapping);
+    }
+}
