c5ec1e3ce179d1ebf9783fc8b169e3c26ff2440b
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-11-03 16:16:46 +0530

    Time: 2 ms (98.95%), Space: 41.9 MB (92.16%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,22 +15,24 @@
  */
 class Solution {
     int preIndex;
-    public int finder(int[] inorder, int element){
-        for(int i=0;i<inorder.length;i++){
-            if(element==inorder[i]) return i;
-        }
-        return -1;
-    }
-    public TreeNode builder(int[] preorder, int[] inorder, int inorderStart, int inorderEnd, int length){
+    // public int finder(int[] inorder, int element){
+    //     for(int i=0;i<inorder.length;i++){
+    //         if(element==inorder[i]) return i;
+    //     }
+    //     return -1;
+    // }
+    public TreeNode builder(int[] preorder, int[] inorder, int inorderStart, int inorderEnd, int length, Map<Integer, Integer> mapping){
         
         if(preIndex>=length || inorderStart>inorderEnd) return null;
         
         int element=preorder[preIndex++];
-        int rootInInorder=finder(inorder, element);
+        // int rootInInorder=finder(inorder, element); 
+        // in optimized version we just take the value from the map in O(1) getting the solution to O(n)
+        int rootInInorder=mapping.get(element);
         
         TreeNode temp= new TreeNode(element);
-        temp.left=builder(preorder, inorder, inorderStart, rootInInorder-1, length);
-        temp.right=builder(preorder, inorder, rootInInorder+1, inorderEnd, length);
+        temp.left=builder(preorder, inorder, inorderStart, rootInInorder-1, length, mapping);
+        temp.right=builder(preorder, inorder, rootInInorder+1, inorderEnd, length, mapping);
         
         return temp;       
         
@@ -38,6 +40,11 @@ class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         preIndex=0;
         int n=preorder.length;
-        return builder(preorder, inorder, 0, n-1, n);
+        //optimization
+        Map<Integer, Integer> mapping= new HashMap<>();
+        for(int i=0;i<n;i++){
+            mapping.put(inorder[i], i);
+        }
+        return builder(preorder, inorder, 0, n-1, n, mapping);
     }
 }
