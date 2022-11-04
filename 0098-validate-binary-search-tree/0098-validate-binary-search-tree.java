9d2ceb7130380e5e3e2d506e447767244ca20051
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-11-04 18:13:01 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (36.49%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,28 +15,34 @@
  */
 class Solution {
     public void inorder(TreeNode root, ArrayList<Integer> path){
+        //O(n)+stack space and O(n) time complexity approach
         if(root==null) return;
         if(root.left!=null) inorder(root.left, path);
         path.add(root.val);
         if(root.right!=null) inorder(root.right, path);
         
     }
-//     public boolean check(TreeNode root){
-//         if(root==null) return true;
+    public boolean check(TreeNode root, int minRange, int maxRange){
         
-//         if(root.left!=null) if(root.left.val>=root.val || !check(root.left)) return false;
+        if(root==null) return true;
         
-//         if(root.right!=null) if(root.right.val<=root.val || !check(root.right)) return false;
+        if(minRange>root.val || root.val>maxRange) return false;
         
-//         return true;
-//     }
-    public boolean isValidBST(TreeNode root) {
-        ArrayList<Integer> path=new ArrayList<>();
-        inorder(root, path);
-        int size=path.size();
-        for(int i=0;i<size-1;i++){
-            if(path.get(i)>=path.get(i+1)) return false;
-        }
+        if(root.left!=null) if(root.left.val>=root.val || !check(root.left, minRange, root.val-1)) return false;
+        
+        if(root.right!=null) if(root.right.val<= root.val || !check(root.right, root.val+1, maxRange)) return false;
+        
         return true;
+    }
+    public boolean isValidBST(TreeNode root) {
+        // ArrayList<Integer> path=new ArrayList<>();
+        // inorder(root, path);
+        // int size=path.size();
+        // for(int i=0;i<size-1;i++){
+        //     if(path.get(i)>=path.get(i+1)) return false;
+        // }
+        // return true;
+        
+        return check(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 }
