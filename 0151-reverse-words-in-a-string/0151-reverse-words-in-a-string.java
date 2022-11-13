9e90781bfdd038c2fc3d5f0934dd37e5bdd90682
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-11-13 16:17:45 +0530

    Time: 13 ms (66.12%), Space: 43.7 MB (55.34%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -17,28 +17,47 @@ class Solution {
         
 //         return reversal(s.substring(index+1))+word;
 //     }
-    public String reverseWords(String s) {
+    
+    // first approach
+    
+//     public String reverseWords(String s) {
+//         // char[] arr=s.toCharArray();
+//         String reverseSentence="";
+//         String word="";
+//         s=s+" ";
         
-        String reverseSentence="";
-        String word="";
-        s=s+" ";
-        
-        for(int i=0;i<s.length()-1;i++){
-            if(s.charAt(i)!=' ' && s.charAt(i+1)==' '){
-                // word has completed
-                // so add word to reverseSentence
-                word+=s.charAt(i);
-                word+=" ";
-                reverseSentence=word+reverseSentence;
-                word="";               
+//         for(int i=0;i<s.length()-1;i++){
+//             if(s.charAt(i)!=' ' && s.charAt(i+1)==' '){
+//                 // word has completed
+//                 // so add word to reverseSentence
+//                 word+=s.charAt(i);
+//                 if(reverseSentence.length()!=0) word+=" ";
+//                 reverseSentence=word+reverseSentence;
+//                 word="";               
                 
                 
-            }
-            else if(s.charAt(i)!=' '){
-                // add char to word
-                word+=s.charAt(i);
-            }
+//             }
+//             else if(s.charAt(i)!=' '){
+//                 // add char to word
+//                 word+=s.charAt(i);
+//             }
+//         }
+//         return reverseSentence;
+//     }
+    public String reverseWords(String s){
+        //Stack Approach + String builder to reduce space to O(1) and O(n) stack.
+    
+        Stack<String> st = new Stack<String>();
+        for (String a : s.trim().split(" ")) {
+            if (!a.isEmpty())
+                st.push(a);
         }
-        return reverseSentence.trim();
+        
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+            sb.append(" ");            
+        }
+        return sb.toString().trim();
     }
 }
