c0c02c8e1cdd24d363f37e3299846887864c3af5
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2022-11-22 08:42:01 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (93.96%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,29 +1,30 @@
 class Solution {
-    // public int hammingDistance(int x, int y) {
-    //     // int count=0;
-    //     // while(x!=0 && y!=0){
-    //     //     if(((x&1)^(y&1))==1){
-    //     //         count++;
-    //     //     }
-    //     //     x>>=1;
-    //     //     y>>=1;
-    //     // }
-    //     // return count;
-    // }
-    
-    public int hammingDistance(int x , int y){
+    public int hammingDistance(int x, int y) {
         int count=0;
-        int xor=x^y;
-        
-        int mask=1;
         for(int i=0;i<32;i++){
-            if((xor&mask)!=0){
+            // System.out.println((x&1)+" "+(y&1));
+            if(((x&1)^(y&1))==1){
                 count++;
             }
-            mask<<=1;
+            x>>=1;
+            y>>=1;
         }
         return count;
     }
+    
+//     public int hammingDistance(int x , int y){
+//         int count=0;
+//         int xor=x^y;
+        
+//         int mask=1;
+//         for(int i=0;i<32;i++){
+//             if((xor&mask)!=0){
+//                 count++;
+//             }
+//             mask<<=1;
+//         }
+//         return count;
+//     }
 }
 
 // Approach : XOR the lsb each time 
