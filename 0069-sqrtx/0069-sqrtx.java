55da78423928e0c1ac68e021e39685c0e4df26dc
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-02-06 02:18:45 -0800

    Time: 1 ms (100.00%), Space: 39.7 MB (58.76%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int mySqrt(int x) {
+//         //approach    O(root(N))  comparing squares of integers to x
+//         long ans=-1;
+//         int s=0;
+//         int e=x;
+//         long mid=s+(e-s)/2;
+//         while(s<=e){
+//             long square=mid*mid;
+//             if(square==x){
+//                 return (int) mid;
+                
+//             }
+//             else if(square>x){
+//                 e=(int)mid-1;
+//             }
+//             else{
+//                 ans=mid;
+//                 s=(int)mid+1;
+//             }
+//             mid=s+(e-s)/2;
+//         }
+//         return (int)ans;
+        int s=0;
+        int e=x;
+        long mid=s+(e-s)/2;
+        long ans=-1;
+        while(s<=e){
+            long square=mid*mid;
+            if(square==x) return (int) mid;
+            if(square>x) e=(int)mid-1;
+            else{
+                ans=mid;
+                s=(int)mid+1;
+            }
+            mid=s+(e-s)/2;
+        }
+        return (int)ans;
+    }
+}
