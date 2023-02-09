5270b3917501a26dcf4d04ee44c4a8b009722a9f
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-02-08 17:28:14 -0800

    Time: 1 ms, Memory: 42.3 MB - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -2,9 +2,7 @@ class Solution {
     public int maxProfit(int[] prices) {
         int profit=0;
         for(int i=0;i<prices.length-1;i++){
-            if(prices[i]<prices[i+1]){
-                profit+=prices[i+1]-prices[i];
-            }
+            profit+=Math.max(0, prices[i+1]-prices[i]);
         }
         return profit;
     }
