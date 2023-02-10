13f4ac94ae2a53b9c7e059b142a8930a1ff62086
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-02-09 23:21:16 -0800

    Time: 24 ms, Memory: 42.5 MB - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -3,7 +3,7 @@ class Solution {
         int n=s.length();
         for(int i=0;i<n;i++){
             char ch=s.charAt(i);
-            if(s.indexOf(ch)==i && s.lastIndexOf(ch)==i) return i;
+            if(s.indexOf(ch)==s.lastIndexOf(ch)) return i;
         }
         return -1;
     }
