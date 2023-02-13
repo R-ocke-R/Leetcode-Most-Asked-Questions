c286a552095173c5207e51dc2ce207dda9223813
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-02-13 07:22:24 -0800

    Time: 0 ms (100.00%), Space: 41.8 MB (67.19%) - LeetHub

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        // a little wierd input
+        // I think the only "approach" could be to push  data values to one step forward
+        while(node.next.next!=null){
+            node.val=node.next.val;
+            node=node.next;
+        }
+        node.val=node.next.val;
+        node.next=null;
+    }
+}
