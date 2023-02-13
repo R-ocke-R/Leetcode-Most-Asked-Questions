387ed5650808121a92ce0ec6f858e0f77693a26d
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-02-12 21:25:28 -0800

    Time: 1 ms, Memory: 40.2 MB - LeetHub

--- implement-strstr/implement-strstr.java
+++ implement-strstr/implement-strstr.java
@@ -13,14 +13,45 @@
 //         }
 //     }
 // }
+// class Solution{
+//     public int strStr(String haystack, String needle) {
+//         // empty needle appears everywhere, first appears at 0 index
+//         if (needle.length() == 0)
+//             return 0;
+//         if (haystack.length() == 0)
+//             return -1;
+        
+        
+//         for (int i = 0; i < haystack.length(); i++) {
+//             // no enough places for needle after i
+//             if (i + needle.length() > haystack.length()) break;
+            
+//             for (int j = 0; j < needle.length(); j++) {
+//                 if (haystack.charAt(i+j) != needle.charAt(j))
+//                     break;
+//                 if (j == needle.length()-1)
+//                     return i;
+//             }
+//         }
+        
+//         return -1;
+//     }
+// }
+
 class Solution{
-    public int strStr(String haystack, String needle) {
-  for (int i = 0; ; i++) {
-    for (int j = 0; ; j++) {
-      if (j == needle.length()) return i;
-      if (i + j == haystack.length()) return -1;
-      if (needle.charAt(j) != haystack.charAt(i + j)) break;
+    public int strStr(String haystack, String needle){
+        // empty needle
+        if(needle.length()==0) return 0;
+        if(haystack.length()==0) return -1;
+        
+        for(int i=0;i<haystack.length();i++){
+            if(i+needle.length()>haystack.length()) break;
+            
+            for(int j=0;j<needle.length();j++){
+                if(needle.charAt(j)!=haystack.charAt(i+j)) break;
+                if(j==needle.length()-1) return i;
+            }
+        }
+        return -1;
     }
-  }
-}
 }
