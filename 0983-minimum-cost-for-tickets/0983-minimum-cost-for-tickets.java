03304eb7ca96b04dd6e6d01a60d7a66ed9684a02
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-04-03 01:34:14 -0700

    Time: 3 ms (25.04%), Space: 42.4 MB (10.71%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,4 +1,13 @@
+class pair{
+        int day;
+        int cost;
+        public pair(int day, int cost){
+            this.day=day;
+            this.cost=cost;
+        }
+    }
 class Solution {
+    
     public int dayIncrement(int[] days, int currIndex, boolean value){
         if (value){
             // increment current index with factor 7
@@ -45,58 +54,7 @@ class Solution {
         
         return dollar;
     }
-    
-    public int mincostTickets(int[] days, int[] costs) {
-        // recursive function call: its working nicely
-        //return function(days, costs, 0);
         
-        //memoization
-        // int[] dp=new int[days.length];
-        // return memo(days, costs, 0, dp);
-        // worked but gave TLE on some testcase on submission.
-        
-        //tabulation
-        return tab(days, costs);
-        
-    }
-    
-    public int tab(int[] days, int[] costs){
-        int n=days.length;
-        int[] dp = new int[n+1];
-        dp[n]=0;
-        
-        for(int index = n-1; index >= 0 ; index--){
-            int dollar=Integer.MAX_VALUE;
-            dollar=Math.min(dollar, costs[0] + dp[index+1]);
-        
-            // seven days
-            dollar=Math.min(dollar, costs[1] + dp[dayIncrement(days, index, true)]);
-
-            // month ticket
-            dollar=Math.min(dollar, costs[2] + dp[dayIncrement(days, index, false)]);
-//             int nextIndex=index+1;            
-//             if(nextIndex < n) dollar=Math.min(dollar, costs[0] + dp[nextIndex]);
-//             else dollar=Math.min(dollar, costs[0]);
-
-//             // seven days
-//             nextIndex=dayIncrement(days, index, true);
-//             if(nextIndex < n) dollar=Math.min(dollar, costs[1] + dp[nextIndex]);
-//             else dollar=Math.min(dollar, costs[1]);
-            
-//             // month ticket
-//             nextIndex=dayIncrement(days, index, false);
-//             if(nextIndex < n) dollar=Math.min(dollar, costs[2] + dp[nextIndex]);
-//             else dollar=Math.min(dollar, costs[1]);
-            
-            
-            dp[index]=dollar;
-        }
-        System.out.println(Arrays.toString(dp));
-        return dp[0];
-    }
-    
-    
-    
     public int memo(int[] days, int[] costs, int index, int[] dp){
         
         if(index==-1 || index==days.length) return 0;
@@ -117,5 +75,65 @@ class Solution {
         
         dp[index]=dollar;
         return dollar;
+    }
+    
+    public int tab(int[] days, int[] costs){
+        int n=days.length;
+        int[] dp = new int[n+1];
+        dp[n]=0;
+        
+        for(int index = n-1; index >= 0 ; index--){
+            int dollar=Integer.MAX_VALUE;
+            dollar=Math.min(dollar, costs[0] + dp[index+1]);
+        
+            dollar=Math.min(dollar, costs[1] + dp[dayIncrement(days, index, true)]);
+
+            dollar=Math.min(dollar, costs[2] + dp[dayIncrement(days, index, false)]);
+            
+            dp[index]=dollar;
+        }
+        return dp[0];
+    }
+    
+    public int mincostTickets(int[] days, int[] costs) {
+        // recursive function call: its working nicely
+        //return function(days, costs, 0);
+        
+        //memoization
+        // int[] dp=new int[days.length];
+        // return memo(days, costs, 0, dp);
+        // worked but gave TLE on some testcase on submission.
+        
+        //tabulation
+        // return tab(days, costs);
+        // worked nicely and easy
+        
+        // space Optimization
+        
+        return space(days, costs);
+        
+    }
+    
+    public int space(int[] days, int[] costs){
+        Queue<pair> week = new LinkedList<>();
+        Queue<pair> month = new LinkedList<>();
+        
+        int ans=0;
+        
+        for(int day : days){
+            // step 1: remove expired days
+            while(!week.isEmpty() && week.peek().day + 7 <=day) week.remove();        
+            while(!month.isEmpty() && month.peek().day + 30 <=day) month.remove();    
+            
+            // step 2: add cost for today in queue
+            week.add(new pair(day, ans+costs[1]));
+            month.add(new pair(day, ans+costs[2]));
+            
+            // update ans
+            ans = Math.min(ans+costs[0], Math.min(week.peek().cost, month.peek().cost));
+        }
+        
+        
+        return ans;
     }
 }
