16ee2cca3822678953d16a9465334ae0646ea2b0
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-04-06 08:27:50 -0700

    Time: 0 ms (100.00%), Space: 39.4 MB (64.12%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -2,13 +2,9 @@ class Solution {
     public int findComplement(int num) {
         int mask=0;
         int set=0;
-        for(int i=31;i>=0;i--){
-            if((num&(1<<i))==(1<<i)){
+        for(int i=31;i>=0 && ((num & 1<<i)!=(1<<i));i--){
                 set=i;
-                break;
-            }
         }
-        // System.out.println(set);
 
         //creating the mask
         while(set--!=0){
