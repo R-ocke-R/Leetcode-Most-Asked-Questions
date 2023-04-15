d1ad42630dac32574d192971bb6b52ec5b56e2ff
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-04-15 10:02:39 -0700

    Time: 60 ms (50.98%), Space: 53.4 MB (26.63%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,6 +1,7 @@
 class Solution {
         
-    public boolean caller(int[] nums, int n, int index, int target){
+    public boolean caller(int[] nums, int n, int index, int target)
+    {
         
         // base
         if(target==0) return true;
@@ -12,7 +13,8 @@ class Solution {
         
         return (Call || addCall);
     }
-    public boolean memo(int[] nums, int index, int target, int[][] dp){
+    public boolean memo(int[] nums, int index, int target, int[][] dp)
+    {
         if(target==0) return true;
         if(target < 0 || index==nums.length) return false;
         
@@ -31,6 +33,29 @@ class Solution {
         
         return ans;
     }
+    public boolean tabulation(int[] nums, int tSum)
+    {
+        // Stp 1: Creating DP and filling base case
+        int n= nums.length;
+        boolean [][] dp = new boolean[n+1][tSum+1];
+        
+        for(int index=0; index<=n; index++){
+            dp[index][0]=true;
+            // all target=0 are true;
+        }
+        for(int index = n-1 ; index >= 0; index--){
+            for(int target = 0; target <= tSum; target ++){
+                boolean addCall = false;
+                if(target-nums[index]>=0) addCall = dp[index+1][target-nums[index]];
+                boolean Call = dp[index+1][target];
+
+                boolean ans= (Call || addCall);
+                dp[index][target]=ans;
+            }
+        }
+        return dp[0][tSum];
+        
+    }
     public boolean canPartition(int[] nums) {
         int n= nums.length;
         int sum=0;
@@ -45,6 +70,7 @@ class Solution {
         // if(sum%2!=0) return false;
         if ((sum & 1) == 1) return false;
         // return caller(nums, n, 0, sum/2);             Recursive call
-        return memo(nums, 0, sum/2, dp);
+        // return memo(nums, 0, sum/2, dp
+        return tabulation(nums, sum/2);
     }
 }
