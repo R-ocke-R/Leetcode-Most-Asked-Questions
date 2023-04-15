88a135c714a217fa4cf71f31ed217f0b5844b709
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-04-15 00:04:18 -0700

    Time: 10 ms (47.83%), Space: 43.4 MB (35.87%) - LeetHub

---
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int recursive(int[] slices, int index, int endIndex, int n)
+    {
+        // base case
+        if(n==0 || index > endIndex) return 0;
+        
+        // consider
+        int take = slices[index] + recursive(slices, index + 2, endIndex, n-1);
+        
+        int notTake = 0 + recursive(slices, index+1, endIndex, n);
+        
+        return Math.max(take, notTake);
+    }
+    public int memo(int[] slices, int index, int endIndex, int n, int[][] dp)
+    {
+        // base case
+        if(n==0 || index > endIndex) return 0;
+        if(dp[index][n]!=0) return dp[index][n];
+        
+        // consider
+        int take = slices[index] + memo(slices, index + 2, endIndex, n-1, dp);
+        
+        int notTake = 0 + memo(slices, index+1, endIndex, n, dp);
+        
+        dp[index][n]=Math.max(take, notTake);
+        return dp[index][n];
+    }
+    public int maxSizeSlices(int[] slices) {
+        // recursive approach
+        // int n= slices.length;
+        // int one = recursive(slices, 0, n-2, n/3);
+        // int two = recursive(slices, 1, n-1, n/3);
+        // return Math.max(one, two);
+        
+        
+        
+        // Memoization
+        int n= slices.length;
+        int[][] dp1 = new int[n][n];
+        int one = memo(slices, 0, n-2, n/3, dp1);
+        
+        int[][] dp2 = new int[n][n];
+        int two = memo(slices, 1, n-1, n/3, dp2);
+        return Math.max(one, two);
+    }
+}
+
+/* 
+
+as always understanding the problem is very crucial, and as we can understand 
+intuitively, this is kind of like house robber question, KIND OF 
+
+particularly HB2 where the houses were circular in arrangement. 
+
+
+in addition to that specific condition, we also have an additional condition
+alice and bob are also picking when you pick, and since we have 3n slices, 
+in all we can only select n slices of pizza... **
+
+*/
