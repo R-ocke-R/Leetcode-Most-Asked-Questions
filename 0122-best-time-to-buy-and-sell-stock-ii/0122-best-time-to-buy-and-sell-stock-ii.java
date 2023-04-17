20e4ff07107d22898d21d3eb619902c43b3ec05f
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-04-17 10:44:02 -0700

    Time: 0 ms (100.00%), Space: 42.1 MB (94.50%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -18,7 +18,11 @@ class Solution {
         // // 0 is false and one is true.
         // return memo(prices, 0, 1, dp);
         
-        return tabulation(prices);
+        // return tabulation(prices);
+        // fast working tabulation approach
+        
+        
+        return space(prices);
     }
     public int recursive(int[] prices, int index, boolean buy )
     {
@@ -85,5 +89,31 @@ class Solution {
         }
         return dp[1][0];
     }
-    
+    public int space(int[] prices){
+        int n = prices.length;
+        int curr0 = 0;
+        int curr1 = 0;
+        int next0 = 0;
+        int next1 = 0;
+        // base is 0 so its automatically filled.
+        for(int index = n-1; index >= 0; index --){
+            for(int buy = 0 ; buy <= 1; buy++){
+                if (buy==1) {
+                    // in buy itself there are two choices.memo
+                    int bought = -(prices[index]) + next0;
+                    int skipB = 0 + next1;
+                    curr1 = Math.max(bought, skipB);
+                }
+                else {
+                    int sell = (prices[index] + next1);
+                    int skipS = 0 + next0;
+                    curr0 = Math.max(sell, skipS);
+                } 
+            }
+            next0=curr0;
+            next1=curr1;
+        }
+        return next1;
+    }
+
 }
