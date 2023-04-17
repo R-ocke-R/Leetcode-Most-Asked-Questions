69cc8c1ff8bb11b148ad1d4a88e3d9b2b4f87b70
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-04-17 10:20:35 -0700

    Time: 3 ms (12.58%), Space: 45.7 MB (5.48%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int maxProfitGreedy(int[] prices) {
+        int profit=0;
+        for(int i=0;i<prices.length-1;i++){
+            profit+=Math.max(0, prices[i+1]-prices[i]);
+        }
+        return profit;
+    }
+    
+    public int maxProfit(int[] prices){
+        // return recursive(prices, 0, true);
+        // recursive solution ran but gave TLE
+        
+        int[][] dp = new int[2][prices.length];
+        Arrays.fill(dp[0], -1);
+        Arrays.fill(dp[1], -1);
+        // Please note that boolean buy also need to behave as an integer for the array.
+        // 0 is false and one is true.
+        return memo(prices, 0, 1, dp);
+    }
+    public int recursive(int[] prices, int index, boolean buy )
+    {
+        
+        // base
+        if(index==prices.length) return 0;        
+        
+        int value = 0;
+        if (buy) {
+            // in buy itself there are two choices.
+            int bought = -(prices[index]) + recursive(prices, index+1, false);
+            int skipB = 0 + recursive(prices, index+1, true);
+            value = Math.max(bought, skipB);
+        }
+        else {
+            int sell = (prices[index] + recursive(prices, index+1, true));
+            int skipS = 0 + recursive(prices, index+1, false);
+            value = Math.max(sell, skipS);
+        }
+        return value;
+    }
+    public int memo(int[] prices, int index, int buy, int[][] dp){
+        if(index==prices.length) return 0;        
+        if(dp[buy][index]!=-1) return dp[buy][index];
+        
+        int value = 0;
+        if (buy==1) {
+            // in buy itself there are two choices.memo
+            int bought = -(prices[index]) + memo(prices, index+1, 0, dp);
+            int skipB = 0 + memo(prices, index+1, 1, dp);
+            value = Math.max(bought, skipB);
+        }
+        else {
+            int sell = (prices[index] + memo(prices, index+1, 1, dp));
+            int skipS = 0 + memo(prices, index+1, 0, dp);
+            value = Math.max(sell, skipS);
+        }
+        dp[buy][index]=value;
+        return dp[buy][index];
+    }
+    
+}
