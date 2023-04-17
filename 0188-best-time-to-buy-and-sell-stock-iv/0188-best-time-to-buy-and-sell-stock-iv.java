55c4aa2e7cfe24e31faf6570f88074c7b9728f29
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-04-17 12:20:49 -0700

    Time: 9 ms (37.47%), Space: 42.8 MB (31.22%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,9 +1,25 @@
 class Solution {
-    public int maxProfit(int k, int[] prices) {
-        int limit = k;
-        return space(prices, limit);
+    public int maxProfit(int k, int[] prices)
+    {
+        // int limit = k;
+        // return space(prices, limit);
+        // changing the limit from last question to this. and reusing the previous-best code. 
+        
+        // in the last question we used 3d DP, and even in space function the space is still not O(1) is O(2k) where k is limit
+        // so we optimise it by merging the parameters.
+        // return newRecursive(prices, 0, 0, k);
+        // working function but slow -> TLE
+        
+        // Memo
+        int n= prices.length;
+        int[][] dp = new int[n][k*2];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return newMemo(prices, 0, 0, k, dp);
     }
-    public int space(int[] prices, int lnt){
+    public int space(int[] prices, int lnt)
+    {
         int n=prices.length;
         int[][][] dp= new int [n+1][2][lnt+1];
         int[][] next = new int[2][lnt+1];
@@ -34,4 +50,44 @@ class Solution {
         return curr[1][lnt];
 
     }
+    public int newRecursive(int[] prices, int index, int operations, int k)
+    {
+        // this function is an attempt to reduce the paramaters, so that while optimising to dp solution we can 
+        // move from 3d to 2d.
+        
+        // base
+        if(index==prices.length) return 0;
+        if(operations==2*k) return 0;
+        int value=0;
+        if(operations%2==0){
+            int bought = -(prices[index]) + newRecursive(prices, index+1, operations+1, k);
+            int skipB = 0 + newRecursive(prices, index+1, operations, k);
+            value = Math.max(bought, skipB);
+        }
+        else {
+            int sell = (prices[index] + newRecursive(prices, index+1, operations+1, k));
+            int skipS = 0 + newRecursive(prices, index+1, operations, k);
+            value = Math.max(sell, skipS);
+        }
+        return value;
+    }
+    public int newMemo(int[] prices, int index, int operations, int k, int[][]dp)
+    {
+        if(index==prices.length || operations==2*k) return 0;
+        if(dp[index][operations]!=-1) return dp[index][operations];
+        int value=0;
+        if(operations%2==0){
+            int bought = -(prices[index]) + newMemo(prices, index+1, operations+1, k, dp);
+            int skipB = 0 + newMemo(prices, index+1, operations, k, dp);
+            value = Math.max(bought, skipB);
+        }
+        else {
+            int sell = (prices[index] + newMemo(prices, index+1, operations+1, k, dp));
+            int skipS = 0 + newMemo(prices, index+1, operations, k, dp);
+            value = Math.max(sell, skipS);
+        }
+        dp[index][operations] = value;
+        return dp[index][operations];
+    }
+        
 }
