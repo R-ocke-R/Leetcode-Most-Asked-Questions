3185895c123abb61943dc41a053e664c4f89ae35
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-04-17 11:43:27 -0700

    Time: 81 ms (34.30%), Space: 63.7 MB (34.47%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -16,8 +16,12 @@ class Solution {
         // return memo(prices, 0, 1, limit, dp);
         
         // moving on to tabulation
-        int limit=2;
-        return tabulation(prices, limit);
+        // int limit=2;
+        // return tabulation(prices, limit);
+        
+        // now to space optimisation
+        int limit = 2;
+        return space(prices, limit);
 
     }
     public int recursive(int[] prices, int index, boolean buy, int limit)
@@ -90,5 +94,36 @@ class Solution {
             }
         }
         return dp[0][1][lnt];
+    }
+    public int space(int[] prices, int lnt){
+        int n=prices.length;
+        int[][][] dp= new int [n+1][2][lnt+1];
+        int[][] next = new int[2][lnt+1];
+        int[][] curr = new int[2][lnt+1];
+        // order [index][buy][limit]
+        // base cases values are already 0 so no shit there
+        
+        for(int index=n-1; index >= 0; index--){
+            for(int buy =0; buy <=1; buy++){
+                for(int limit = 1; limit <= lnt; limit++){
+                    int value = 0;
+                    if (buy==1) {
+                        // in buy itself there are two choices.
+                        int bought = -(prices[index]) + next[0][limit];
+                        int skipB = 0 + next[1][limit];
+                        value = Math.max(bought, skipB);
+                    }
+                    else {
+                        int sell = (prices[index] + next[1][limit-1]);
+                        int skipS = 0 + next[0][limit];
+                        value = Math.max(sell, skipS);
+                    }
+                    curr[buy][limit] = value;
+                }
+            }
+            next = curr;
+        }
+        return curr[1][lnt];
+
     }
 }
