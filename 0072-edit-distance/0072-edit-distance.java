5cb0b68d47433b2311e0fc48cf4a667148faa1b2
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2023-04-18 10:07:26 -0700

    Time: 6 ms (71.78%), Space: 42.3 MB (84.68%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -42,12 +42,45 @@ class Solution {
         dp[i][j]=ans;
         return dp[i][j];
     }
+    public int tabulation(String word1, String word2){
+        int n=word1.length();
+        int m= word2.length();
+        int[][] dp = new int[n+1][m+1];
+        
+        // filling the base cases
+        for(int i=0; i<m; i++){
+            dp[n][i]=m-i;
+        }
+        for(int j=0;j<n;j++){
+            dp[j][m]=n-j;
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int ans = 0;
+                if(word1.charAt(i)==word2.charAt(j)) ans = dp[i+1][j+1];
+        
+                else {
+                    ans = 1 + dp[i][j+1];
+                    ans = Math.min(ans, 1 + dp[i+1][j]);
+                    ans = Math.min(ans, 1 + dp[i+1][j+1]);
+                }
+                dp[i][j]=ans; 
+            }
+        }
+        return dp[0][0];
+        
+        
+    }
     public int minDistance(String word1, String word2) {
         // return recursive(word1, word2, 0, 0)
         // recursive TLE
         
         //memo
-        int[][] dp = new int[word1.length()][word2.length()];
-        return memo(word1, word2, 0, 0, dp);
+        // int[][] dp = new int[word1.length()][word2.length()];
+        // return memo(word1, word2, 0, 0, dp);
+        
+        //tabulation
+        return tabulation(word1, word2);
     }
 }
