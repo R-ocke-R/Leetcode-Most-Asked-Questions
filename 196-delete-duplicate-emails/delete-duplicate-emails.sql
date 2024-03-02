c16bd958df9f30987c3697986e10c1e75314cbda
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-03-01 23:48:18 -0800

    Time: 1714 ms (31.03%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -4,5 +4,16 @@
 -- group by email
 -- having count(email) > 1;
 
-delete p1 from person p1,person p2 
-where p1.email=p2.email and p1.id>p2.id;
+-- delete p1 from person p1,person p2 
+-- where p1.email=p2.email and p1.id>p2.id;
+
+delete from 
+Person
+where  
+Id not in (select Id 
+           from 
+            (select min(Id) as Id 
+             from Person 
+             group by Email
+            ) p
+          );
