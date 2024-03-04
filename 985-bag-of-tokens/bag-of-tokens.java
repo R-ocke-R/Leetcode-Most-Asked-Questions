6c5f805c3d1f34fbce76c4f4d0db0864b2b82bd2
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-03-04 11:26:59 -0800

    Time: 5 ms (5.73%) | Memory: 42.6 MB (73.25%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        int low = 0;
+        int high = tokens.length - 1;
+        int score = 0;
+        Arrays.sort(tokens);
+
+        while (low <= high) {
+            // When we have enough power, play lowest token face-up
+            if (power >= tokens[low]) {
+                score += 1;
+                power -= tokens[low];
+                low += 1;
+            // We don't have enough power to play a token face-up
+            // If there is at least one token remaining,
+            // and we have enough score, play highest token face-down
+            } else if (low < high && score > 0) {
+                score -= 1;
+                power += tokens[high];
+                high -= 1;
+            // We don't have enough score, power, or tokens 
+            // to play face-up or down and increase our score
+            } else {
+                return score;
+            }
+        }
+        return score;
+    }
+}
+
+/* chance one does calls for playing all the tokens[i] that are smaller than power, pllay them face up, gain the score.
+
+... there are a lot of permutations available. 
+*/
