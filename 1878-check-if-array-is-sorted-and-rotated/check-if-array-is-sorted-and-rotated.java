09f969b3fe26ddc515d975b8583731e18e90cc69
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-03-04 00:48:44 -0800

    Time: 0 ms (100.00%) | Memory: 40.9 MB (90.13%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int count =0;
+        for(int i =1; i< nums.length;i++){
+            if(nums[i-1]>nums[i]) {
+                count ++;
+            }
+        }
+        if(nums[0]<nums[nums.length-1]) count++;
+        if(count <2)  return true;
+        return false;
+    }
+
+}
