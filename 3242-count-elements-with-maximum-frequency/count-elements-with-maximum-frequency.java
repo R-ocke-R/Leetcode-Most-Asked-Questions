ef4883ebaa49ed077aa0f2f894b4790be94cfb51
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-03-07 21:22:21 -0800

    Time: 1 ms (99.64%) | Memory: 41.8 MB (97.32%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -1,9 +1,8 @@
 class Solution {
-    public int maxFrequencyElements(int[] nums) {
+    public int maxFrequencyElementsTwoPass(int[] nums) {
         // for the brute solution i can use a data structure to store key value frequencies
-        // then sort on values, then store max freq count in a variable.
+        // then store max freq count in a variable.
 
-        // 
         int[] freq = new int[101];
         int maxfreq = 0;
         for(int i =0;i<nums.length;i++){
@@ -14,8 +13,34 @@ class Solution {
         for(int i=1;i<101;i++){
             if(freq[i]==maxfreq) count+=maxfreq;
         }
-        //System.out.println(maxfreq);
         return count;
 
+    }
+    /**  this here is my brute force solution, I took a 100 length array as a frequency storing DS, 
+    I got the length as 100 from the constraints. then in the first pass I also maintained a variable 
+    which can track the maxfreq that we have found till now.
+    now we pass again to calculate the count of that maxfreq. 
+    TC: o(n) SC: O(n) or o(100)=O(1)
+    */
+    public int maxFrequencyElements(int[] nums){
+        // let's try to solve this in one pass; all I have to do is merge the function of the two passes above.
+        int[] freq = new int[101];
+        int maxfreq = 0;
+        int count =0;
+        for(int i =0;i<nums.length;i++){
+            freq[nums[i]]+=1;
+            if(freq[nums[i]]>maxfreq){
+                // we have found a new maximumfreq
+                maxfreq = freq[nums[i]];
+                count = maxfreq;
+                // count has now been reset
+            }
+            else if(freq[nums[i]]==maxfreq){
+                // we have found another instance of maxfreq
+                count += maxfreq;
+            }
+            else {continue;}
+        } 
+        return count;
     }
 }
