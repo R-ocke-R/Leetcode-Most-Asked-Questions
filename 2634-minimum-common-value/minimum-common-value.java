f4d418e5cf6929ccb4cf5d26ae416d366605d792
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-03-08 23:36:39 -0800

    Time: 7 ms (34.44%) | Memory: 62.6 MB (47.63%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -29,9 +29,30 @@ class Solution {
             }
         }
         return -1;
+    }
+    public boolean binarySearch(int[] arr, int ele){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]==ele) return true;
+            else if(arr[mid]>ele) end = mid-1;
+            else start = mid+1;
+        }
+        return false;
+    }
+    public int getCommonBS(int[] nums1, int[] nums2){
+        if(nums1.length>nums2.length) return getCommonBS(nums2, nums1);
 
+        // iterate the smaller array
+
+        for(int ele : nums1){
+            if(binarySearch(nums2, ele)) return ele;
+        }
+
+        return -1;
     }
     public int getCommon(int[] nums1, int[] nums2){
-        return getCommonBetter(nums1, nums2);
+        return getCommonBS(nums1, nums2);
     }
 }
