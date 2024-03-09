3af907bcdfc6c93a28feeda566656d90ac042220
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-03-08 23:11:30 -0800

    Time: 1 ms (96.67%) | Memory: 62 MB (97.41%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int getCommon(int[] nums1, int[] nums2) {
+    public int getCommonBrute(int[] nums1, int[] nums2) {
         int n =nums1.length;
         int m = nums2.length;
 
@@ -13,5 +13,25 @@ class Solution {
             }
         }
         return -1;
+    }
+    public int getCommonBetter(int[] nums1, int[] nums2){
+        // how can I improve the code above. j is running from 0 each time. not needed. 
+        int n =nums1.length;
+        int m = nums2.length;
+        int j=0;
+        
+        for(int i=0;i<n;i++){
+            int ele = nums1[i];
+            // we find this ele in nums. we find it we return as nums1 is sorted in non dec.
+            while(j<m && nums2[j]<=ele){
+                if(nums2[j]==ele ) return ele;
+                j++;
+            }
+        }
+        return -1;
+
+    }
+    public int getCommon(int[] nums1, int[] nums2){
+        return getCommonBetter(nums1, nums2);
     }
 }
