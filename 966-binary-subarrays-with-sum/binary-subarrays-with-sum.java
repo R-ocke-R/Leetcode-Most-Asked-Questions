f8e7554f68d70265a1f3413ff7b907cb3b76bdf7
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-03-28 22:41:30 -0700

    Time: 1 ms (100.00%) | Memory: 47.2 MB (91.32%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
+    public int numSubarraysWithSumBrute(int[] nums, int goal) {
         int count = 0;
         for(int i = 0 ; i< nums.length; i++){
             int currSum = 0;
@@ -27,4 +27,37 @@ class Solution {
         }
         return count;
     }
+    public int numSubarraysWithSum(int[] nums, int goal){
+        return numSubarraysWithLessSum(nums, goal) - numSubarraysWithLessSum(nums , goal - 1);
+    }
+    public int numSubarraysWithLessSum(int[] nums, int goal){
+        if(goal < 0 ) return 0;
+        int n = nums.length;
+        int count = 0;
+        int currSum = 0;
+
+        int left = 0;
+        int right = 0;
+
+        while( right < n ){
+            int val = nums[right];
+
+            currSum += val;
+
+            while(currSum > goal){
+                currSum  -= nums[left++];
+            }
+
+            count += right - left + 1    ;
+
+            right ++;
+
+        }
+
+
+        return count;
+    }
+    // public int numSubarraysWithSum(int[] nums, int goal){
+
+    // }
 }
