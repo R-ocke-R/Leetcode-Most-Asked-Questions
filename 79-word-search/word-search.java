2854ee7474f1a39a636644266b2a505a26cc1ae3
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-04-02 23:40:24 -0700

    Time: 161 ms (41.32%) | Memory: 41.4 MB (61.18%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,65 @@
+class Solution {
+    public boolean inBound(char[][] board, int i, int j){
+        // checks if the move is still in boundary 
+        int n = board.length;
+        int m = board[0].length;
+        if( 0 <= i && i < n && 0 <= j && j < m) return true;
+        return false;
+        
+
+    }
+    public boolean backTrack(char[][] board, String word, int i , int j, int wordIndex){
+        // base case: word found.
+        if(wordIndex == word.length()) return true;
+        // find we the char at wordIndex can be found by moving down
+        if(inBound(board, i+1, j) && board[i+1][j] == word.charAt(wordIndex)){
+            // make the move
+            board[i+1][j] ='#';
+            if(backTrack(board, word, i+1, j, wordIndex+1)) return true;
+            board[i+1][j] = word.charAt(wordIndex);
+        }
+        // find we the char at wordIndex can be found by moving up
+        if(inBound(board, i-1, j) && board[i-1][j] == word.charAt(wordIndex)){
+            // make the move
+            board[i-1][j] ='#';
+            if(backTrack(board, word, i-1, j, wordIndex+1)) return true;
+            board[i-1][j] = word.charAt(wordIndex);
+        }
+        // find we the char at wordIndex can be found by moving left
+        if(inBound(board, i, j-1) && board[i][j-1] == word.charAt(wordIndex)){
+            // make the move
+            board[i][j-1] ='#';
+            if(backTrack(board, word, i, j-1, wordIndex+1)) return true;
+            board[i][j-1] = word.charAt(wordIndex);
+        }
+        // find we the char at wordIndex can be found by moving right
+        if(inBound(board, i, j+1) && board[i][j+1] == word.charAt(wordIndex)){
+            // make the move
+            board[i][j+1] ='#';
+            if(backTrack(board, word, i, j+1, wordIndex+1)) return true;
+            board[i][j+1] = word.charAt(wordIndex);
+        }
+
+        return false;
+    }
+    public boolean exist(char[][] board, String word) {
+        int n = board.length;
+        int m = board[0].length;
+
+
+
+        for(int i = 0; i < n ; i++){
+            for(int j = 0; j < m; j++){
+                char ch = board[i][j];
+                // if this element at i, j is the first char of word then we call if word can be formed here.
+                if(ch == word.charAt(0)) {
+                    board[i][j]='#';
+                    if(backTrack(board, word, i, j, 1)) return true;
+                    board[i][j] =word.charAt(0);
+                }
+                // else continue to find other occurances of 1st character.
+            }
+        }
+        return false;
+    }
+}
