52ce37ea3c4db6d221bf493a3267633694b194a3
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-04-02 23:53:42 -0700

    Time: 151 ms (51.10%) | Memory: 41.3 MB (74.16%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -5,8 +5,17 @@ class Solution {
         int m = board[0].length;
         if( 0 <= i && i < n && 0 <= j && j < m) return true;
         return false;
-        
+    }
+    public boolean backTrackbetter(char[][] board, String word, int i , int j , int wordIndex){
+        if(wordIndex == word.length()) return true;
 
+        board[i][j] = '#';
+        if((inBound(board, i+1, j) && board[i+1][j] == word.charAt(wordIndex) && backTrackbetter(board, word, i+1, j, wordIndex+1)) ||
+            (inBound(board, i-1, j) && board[i-1][j] == word.charAt(wordIndex) && backTrackbetter(board, word, i-1, j, wordIndex+1)) ||
+            (inBound(board, i, j-1) && board[i][j-1] == word.charAt(wordIndex) && backTrackbetter(board, word, i, j-1, wordIndex+1)) ||
+            (inBound(board, i, j+1) && board[i][j+1] == word.charAt(wordIndex) && backTrackbetter(board, word, i, j+1, wordIndex+1))) return true;
+        board[i][j] = word.charAt(wordIndex-1);
+        return false;
     }
     public boolean backTrack(char[][] board, String word, int i , int j, int wordIndex){
         // base case: word found.
@@ -52,12 +61,19 @@ class Solution {
             for(int j = 0; j < m; j++){
                 char ch = board[i][j];
                 // if this element at i, j is the first char of word then we call if word can be formed here.
-                if(ch == word.charAt(0)) {
-                    board[i][j]='#';
-                    if(backTrack(board, word, i, j, 1)) return true;
-                    board[i][j] =word.charAt(0);
-                }
+            
+                // calling the backtrack1 function
+
+                // if(ch == word.charAt(0)) {
+                //     board[i][j]='#';
+                //     if(backTrack(board, word, i, j, 1)) return true;
+                //     board[i][j] =word.charAt(0);
+                // }
                 // else continue to find other occurances of 1st character.
+
+
+                // calling the better function
+                if( ch == word.charAt(0) && backTrackbetter(board, word, i, j, 1)) return true;
             }
         }
         return false;
