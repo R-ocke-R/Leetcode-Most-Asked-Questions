feebdb666c9f41f0915e8534878fd9b8e77f181a
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-04-03 09:44:54 -0700

    Time: 50 ms (94.65%) | Memory: 56.8 MB (97.73%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,17 +1,47 @@
 class Solution {
-    public int maxSubarrayLength(int[] nums, int k) {
-int ans = 0, start = -1;
-        Map<Integer, Integer> frequency = new HashMap();
-        
-        for (int end = 0; end < nums.length; end++) {
-            frequency.put(nums[end], frequency.getOrDefault(nums[end], 0) + 1);
-            while (frequency.get(nums[end]) > k) {
-                start++;
-                frequency.put(nums[start], frequency.get(nums[start]) - 1);
+    public int maxSubarrayLengthBrute(int[] nums, int k) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        int n = nums.length;
+        int maxL = 0;
+
+        for(int i = 0; i < n; i++){
+            freqMap = new HashMap<>();
+            for(int j = i; j< n; j++){
+                int num = nums[j];
+                int freq = freqMap.getOrDefault(num, 0) + 1;
+                freqMap.put(num, freq);
+                if(freq <= k) maxL =Math.max(maxL, j-i+1);
+                else break;
             }
-            ans = Math.max(ans, end - start);
+            
         }
-        
-        return ans;
+        return maxL;
+    }
+    public int maxSubarrayLength(int[] nums, int k) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        int n = nums.length;
+        int maxL = 0;
+
+        int right = 0;
+        int left = 0;
+        while(right < n){
+            int val = nums[right];
+            int freq = freqMap.getOrDefault(val, 0) + 1;
+            freqMap.put(val, freq);
+
+            while(left <= right && freq > k) {
+                // move left pointer. till we are able to remove one occurance of the curent val
+                int value = nums[left++];
+                int leftFreq =freqMap.getOrDefault(value, 0) -1;
+                freqMap.put(value, leftFreq);
+
+                if(value == val) freq = leftFreq;
+
+            }
+            if(freq <= k) maxL = Math.max(maxL, right - left + 1);
+            right ++;
+        }
+    
+        return maxL;
     }
 }
