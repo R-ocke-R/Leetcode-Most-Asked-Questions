cf0b33e12fd1e78f565123a4a795871d56aac831
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-04-04 06:40:53 -0700

    Time: 4 ms (99.92%) | Memory: 66.7 MB (5.47%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.java
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,22 +1,32 @@
 class Solution {
     public long countSubarrays(int[] nums, int k) {
-        int maxElement = Arrays.stream(nums).max().getAsInt();
-        long ans = 0;
-        int start = 0, maxElementsInWindow = 0;
+        int maxEle = nums[0];
+        int n = nums.length;
 
-        for (int end = 0; end < nums.length; end++) {
-            if (nums[end] == maxElement) {
-                maxElementsInWindow++;
-            }
-            while (k == maxElementsInWindow) {
-                if (nums[start] == maxElement) {
-                    maxElementsInWindow--;
-                }
-                start++;
-            }
-            ans += start;
+        for (int i = 1; i < n; i++) {
+            maxEle = Math.max(maxEle, nums[i]);
         }
+        int freq = 0;
+        long count = 0;
 
-        return ans;
+        int right = 0;
+        int left = 0;
+        while(right < n){
+
+            int val = nums[right];
+            if(val == maxEle) freq ++;
+
+            while(k == freq) {
+                int value = nums[left++];
+                if(value == maxEle) freq--;
+            }
+            // left window index value indicates the number of subarrays that can end at right 
+            // as all those will have the condition satisfied. as left increase and move the right half
+            // that indicates that any successful window formed will contribute a lot of subarray
+            // that satisfy that condition where the subarray's will be from left(0 to n) -> right.
+            count += left;
+            right++;
+        }
+        return count;
     }
 }
