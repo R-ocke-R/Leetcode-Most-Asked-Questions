cee6fbb3415813442876a5ccd0e228cb4b4aef6b
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-04-07 02:45:17 -0700

    Time: 0 ms (100.00%) | Memory: 41.2 MB (73.12%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int openCount = 0;
+        int closeCount = 0;
+        int length = s.length() - 1;
+        
+        // Traverse the string from both ends simultaneously
+        for (int i = 0; i <= length; i++) {
+            // Count open parentheses or asterisks
+            if (s.charAt(i) == '(' || s.charAt(i) == '*') {
+                openCount++;
+            } else {
+                openCount--;
+            }
+            
+            // Count close parentheses or asterisks
+            if (s.charAt(length - i) == ')' || s.charAt(length - i) == '*') {
+                closeCount++;
+            } else {
+                closeCount--;
+            }
+            
+            // If at any point open count or close count goes negative, the string is invalid
+            if (openCount < 0 || closeCount < 0) {
+                return false;
+            }
+        }
+        
+        // If open count and close count are both non-negative, the string is valid
+        return true;
+    }
+}
