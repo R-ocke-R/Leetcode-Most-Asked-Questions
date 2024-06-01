a7361cbc823f491cc8ca736e703213f6201a5885
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-06-01 21:58:49 +0530

    Time: 1 ms (99.97%) | Memory: 41.4 MB (17.59%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.java
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.java
@@ -1,22 +1,31 @@
 class Solution {
-    public int countTriplets(int[] arr) {
+    public int countTripletsBruteBetter(int[] arr) {
         int count  = 0 ;
         int n = arr.length;
 
 
         for(int i = 0; i < n; i++){
+            int a = 0;
             for(int j = i+1; j < n ; j++){
+                a ^= arr[j-1];
+                int b =0;
                 for(int k = j; k < n; k++){
                     // all triplets will be created here
-                    int a=0, b=0;
-                    for(int l = i; l < j; l++){
-                        a ^= arr[l];
-                    }
-                    for(int l = j; l < k+1; l++){
-                        b^= arr[l];
-                    }
+                    b^=arr[k];
                     if(a==b) count++;
                 }
+            }
+        }
+        return count;
+    }
+    public int countTriplets(int[] arr) {
+        int count  = 0 ;
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            int a = arr[i];
+            for(int j = i+1; j < n ; j++){
+                a ^= arr[j];
+                if(a==0) count += j-i;
             }
         }
         return count;
