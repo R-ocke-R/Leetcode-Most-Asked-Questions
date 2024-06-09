4d2a453bac7cdaf19617ca548a3a09f19b92b9e9
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-06-10 01:01:29 +0530

    Time: 2 ms (99.20%) | Memory: 54.8 MB (70.18%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -1,30 +1,17 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        /** this question can be solved via 1) DP 2) sliding window
-        tbh this is a very basic sliding window question,
-        right now doing this as part of sliding window playlist by striver.  */
-        int lsum = 0;
-        int rsum = 0;
-        int maxSum = 0;
         int n = cardPoints.length;
-
-        // first create complete left window 
-        for(int i = 0 ; i < k ; i++){
-            lsum += cardPoints[i];
+        int sum = 0;
+        for(int i = 0; i < k; i++){
+            sum += cardPoints[i];
         }
-
-        if( n == k ) return lsum;
-
-        maxSum = lsum;
-        // then we shrink left window
-        // whilest expading right window
-        for(int i = 0; i < k ; i++ ){
-            lsum -= cardPoints[k-i-1];
-            rsum += cardPoints[n-i-1];
-            maxSum = Math.max(lsum+rsum, maxSum);
+        int maxSum = sum;
+        if(k==n) return maxSum;
+        for(int i = k-1; i >=0; i--){
+            sum -= cardPoints[i];
+            sum += cardPoints[--n];
+            maxSum = Math.max(sum, maxSum);
         }
-
         return maxSum;
-        
     }
 }
