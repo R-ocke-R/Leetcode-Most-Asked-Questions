e2849d5d6d47429f692e69bbcf7e5c4ebb4271d1
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-06-10 23:12:32 +0530

    Time: 5 ms (11.65%) | Memory: 41.2 MB (65.80%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,8 +1,31 @@
 class Solution {
+    public void bubbleSort(){}
+    public void mergeSort(){}
+    public void countSort(int[] arr){
+        int n = arr.length;
+        int minVal = Arrays.stream(arr).min().getAsInt();
+        int maxVal = Arrays.stream(arr).max().getAsInt();
+
+        HashMap<Integer, Integer> counts = new HashMap<>();
+        for(int val: arr){
+            counts.put(val, counts.getOrDefault(val, 0) +1);
+        }
+
+        int index = 0;
+        for(int val = minVal; val <= maxVal; ++val){
+            while(counts.getOrDefault(val, 0) > 0){
+                arr[index] = val;
+                index += 1;
+                counts.put(val, counts.get(val)-1);
+            }
+        }
+        
+    }
     public int heightChecker(int[] heights) {
         int n = heights.length;
         int[] copy = Arrays.copyOf(heights, n);
-        Arrays.sort(copy);
+        // Arrays.sort(copy);
+        countSort(copy);
 
         int count = 0;
         for(int i = 0; i < n; i++){
