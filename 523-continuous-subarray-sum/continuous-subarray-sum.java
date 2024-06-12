c8bfa926dbc282565cf58809c1481320e6b0f213
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-06-12 10:21:41 +0530

    Time: 22 ms (67.64%) | Memory: 57.9 MB (45.38%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -1,26 +1,50 @@
-import java.util.HashMap;
-
 class Solution {
+    public boolean checkSubarraySumBrute(int[] nums, int k) {
+        int n = nums.length;
 
-    public boolean checkSubarraySum(int[] nums, int k) {
-        int prefixMod = 0;
-        HashMap<Integer, Integer> modSeen = new HashMap<>();
-        modSeen.put(0, -1);
+        int currSum = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            prefixMod = (prefixMod + nums[i]) % k;
-
-            if (modSeen.containsKey(prefixMod)) {
-                // ensures that the size of subarray is at least 2
-                if (i - modSeen.get(prefixMod) > 1) {
+        for(int i = 0; i < n; i++){
+            currSum = nums[i];
+            for(int j = i+1; j < n; j++){
+                currSum += nums[j];
+                if(currSum % k == 0){
                     return true;
                 }
-            } else {
-                // mark the value of prefixMod with the current index.
-                modSeen.put(prefixMod, i);
             }
         }
-
         return false;
     }
+    public boolean checkSubarraySum(int[] nums, int k){
+        int n = nums.length;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int preSum = 0;
+
+        map.put(0, -1);
+
+        for(int i = 0; i < n; i++){
+            preSum = (preSum + nums[i]) % k;
+
+            if(map.containsKey(preSum)){
+                if( i - map.get(preSum) > 1){
+                    return true;
+                }
+            }else {
+                    // mark the current index
+                    map.put(preSum, i);
+                }
+            }
+        return false;
+        }
+    
 }
+
+/**
+Brute - generated all the subarrays in O(n^2) time complexity, gave TLE.
+
+Optimisation though - in this question there is no way we can optimise it without an hashmap, 
+as an simple left-right sliding window will not work due to the underlying condition.\
+
+ */
