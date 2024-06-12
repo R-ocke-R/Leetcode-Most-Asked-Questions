5f97db16601edb2eca7f6c2c28a7f0fd4c111de8
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-06-12 08:17:01 +0530

    Time: 11 ms (6.79%) | Memory: 43.7 MB (9.24%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,29 +1,42 @@
 class Solution {
-    public void swap(int[] arr, int i, int j){
-        int temp = arr[i];
-        arr[i]=arr[j];
-        arr[j]= temp;
+    public void swap(int[] nums, int i, int j){
+        int temp  = nums[i];
+        nums[i] = nums[j];
+        nums[j]= temp;
+        return;
     }
     public void sortColors(int[] nums) {
-        // we have 0's and 1's and 2's .
-        int n=nums.length;
-        int zero =0;
-        int two = n-1;
-        for(int index = 0; index <= two; index++){
-            if(nums[index]==0 && index != zero){
-                // this means we have the next zero that needs to go 
-                // at its place which is defined by the variable zero.
-                swap(nums, index, zero);
-                zero++;index--;
+        // DNF
+        // brute sort
+        // better count and replace. - two pass solution + constant space
+        // best approach is using DNF technique. 
+
+        int n = nums.length;
+        int left = 0;
+        int right = n-1;
+
+        int index = 0;
+
+        while(index <= right){
+            if(nums[index] == 0){
+                // no swap needed
+                if(index!=left) swap(nums, left, index);
+                left++;
+                index++;
+                
             }
-            else if(nums[index]==2){
-                // again we have two cases when we encounter the next 2 in 
-                // the array, either we need to swap it with the element that is at
-                // variable index two. or if we have reached the index denoted by 
-                // two, we have solved the problem.
-                swap(nums, index, two);
-                two--;index--;
+            else if(nums[index] == 1){
+                //swap(nums, left, index);
+                // left remains the same as it indicates the end of zero
+                index++;
             }
+            else{
+                swap(nums, right, index);
+                // index remains the same.
+                right --;
+            }
+            System.out.println(Arrays.toString(nums));
         }
+        
     }
 }
