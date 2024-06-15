a89f77e85fef3653cc95c9badd5f7b76bb89d1c8
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-06-15 13:50:03 +0530

    Time: 52 ms (87.26%) | Memory: 68.5 MB (80.00%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -1,24 +1,27 @@
 class Solution {
-   // Finds the minimum number of arrows needed to burst all balloons
-   public int findMinArrowShots(int[][] points) {
-        // Sort the balloons by the end position of the arrow
-        Arrays.sort(points, (a, b) -> Integer.compare(a[1],b[1]));
-        
-        // Initialize the number of arrows needed to 1
-        int result = 1;
-        // Set the index of the previously burst balloon to 0
-        int previous = 0;
-        
-        // Iterate through all balloons
-        for(int current = 1; current < points.length; current++){
-            // If the start position of the current balloon is after the end position of the arrow that burst the previous balloon,
-            // then a new arrow is needed to burst the current balloon
-            if(points[current][0] > points[previous][1]){
+    public int findMinArrowShots(int[][] points) {
+        int result  = 1;
+        int n = points.length;
+
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int prev = 0;
+
+        for(int current = 1; current < n ; current ++){
+            if(points[prev][1] < points[current][0]){
                 result++;
-                // Update the index of the previously burst balloon
-                previous = current;
+                // new arrow needed
+                prev = current;
             }
+
         }
-        return result;            
+
+
+
+
+        return result;
+
     }
 }
+/** kinda merge intervals but kinda not - coz its ballons which can't be merged as they have been tied out
+ */
