f5b2485569e6831abe4943b95f9c7ceef5e3e024
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-06-16 14:59:21 +0530

    Time: 3 ms (82.20%) | Memory: 45.4 MB (47.60%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -10,15 +10,14 @@ class Solution {
                 candy[i] = candy[i-1]+1;
             }
         }
+        int sum = candy[n-1];
         for(int i = n-2; i >= 0 ; i--){
             if(ratings[i]>ratings[i+1]){
                 candy[i] = Math.max(candy[i], candy[i+1]+1);
             }
+            sum += candy[i];
         }
-        int sum = 0;
-        for(int i = 0; i< n; i++){
-            sum+= candy[i];
-        }
+        
         return sum;
     }
 }
