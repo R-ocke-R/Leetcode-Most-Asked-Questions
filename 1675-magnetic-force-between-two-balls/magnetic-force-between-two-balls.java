cd5a295208bb98ba2873a7813f369a883fd0db82
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-06-29 20:22:10 +0530

    Time: 35 ms (98.83%) | Memory: 55.8 MB (53.45%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -1,45 +1,33 @@
 class Solution {
+    private boolean placer(int[] position, int m, int mid){
+        int prevBallPosition = position[0];
+        int balls = 1;
 
-    // Check if we can place 'm' balls at 'position'
-    // with each ball having at least 'x' gap.
-    private boolean canPlaceBalls(int x, int[] position, int m) {
-        // Place the first ball at the first position.
-        int prevBallPos = position[0];
-        int ballsPlaced = 1;
-
-        // Iterate on each 'position' and place a ball there if we can place it.
-        for (int i = 1; i < position.length && ballsPlaced < m; ++i) {
+        for(int i = 0; i< position.length && balls < m; i++){
             int currPos = position[i];
-            // Check if we can place the ball at the current position.
-            if (currPos - prevBallPos >= x) {
-                ballsPlaced += 1;
-                prevBallPos = currPos;
+            if(currPos - prevBallPosition >= mid){
+                //place a ball
+                prevBallPosition = currPos;
+                balls ++;
             }
         }
-        // If all 'm' balls are placed, return 'true'.
-        return ballsPlaced == m;
+        return balls == m;
     }
-
     public int maxDistance(int[] position, int m) {
-        int answer = 0;
         int n = position.length;
+        int answer = 0;
         Arrays.sort(position);
 
-        // Initial search space.
         int low = 1;
-        int high = (int) Math.ceil(position[n - 1] / (m - 1.0));
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            // If we can place all balls having a gap at least 'mid',
-            if (canPlaceBalls(mid, position, m)) {
-                // then 'mid' can be our answer,
+        int end = (int) Math.ceil(position[n-1]/(m-1.0));
+
+        while(low <= end){
+            int mid = low + (end-low)/2;
+            if(placer(position, m, mid)){
                 answer = mid;
-                // and discard the left half search space.
                 low = mid + 1;
-            } else {
-                // Discard the right half search space.
-                high = mid - 1;
             }
+            else end = mid -1;
         }
         return answer;
     }
