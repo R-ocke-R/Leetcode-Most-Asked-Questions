930b8fa433e6585a057293e3dccbfd501a5ad2e6
R-ocke-R <72220114+R-ocke-R@users.noreply.github.com>
2024-06-29 18:15:01 +0530

    Time: 22 ms (39.40%) | Memory: 59.5 MB (5.62%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,46 +1,64 @@
 class Solution {
-
-    // Return the number of maximum bouquets that can be made on day mid.
-    private int getNumOfBouquets(int[] bloomDay, int mid, int k) {
-        int numOfBouquets = 0;
-        int count = 0;
-
-        for (int i = 0; i < bloomDay.length; i++) {
-            // If the flower is bloomed, add to the set. Else reset the count.
-            if (bloomDay[i] <= mid) {
-                count++;
-            } else {
-                count = 0;
+    public boolean bloom(int[] bloomDay, int m, int k, int Day){
+        int bouquets= 0;
+        for(int i = 0; i < bloomDay.length - k+1; i++){
+            int flowers = 0;
+            for(int j = i; j < i+k; j++){
+                if(bloomDay[j] <= Day){
+                    // I have a bloomed flower 
+                    flowers ++;
+                }
+            }
+            // check if flowers == k
+            if(flowers==k) {
+                bouquets ++;
+                i += k-1; // skip the picked flowers. 
             }
 
-            if (count == k) {
-                numOfBouquets++;
-                count = 0;
-            }
+            if(bouquets==m) return true;
         }
-
-        return numOfBouquets;
+        return false;
     }
-
-    public int minDays(int[] bloomDay, int m, int k) {
-        int start = 0;
-        int end = 0;
-        for (int day : bloomDay) {
-            end = Math.max(end, day);
-        }
-
-        int minDays = -1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-
-            if (getNumOfBouquets(bloomDay, mid, k) >= m) {
-                minDays = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+    public boolean boom(int[] bloomDay, int m, int k, int Day){
+        int bouquets = 0;
+        int count = 0;
+        for(int i =0; i < bloomDay.length; i++){
+            if(bloomDay[i] <= Day) count ++;
+            else {
+                count = 0;
             }
+            if(count == k ) {
+                bouquets ++;
+                count = 0;
+            }
+            if(bouquets >=m) return true;
+        }
+        if(bouquets >=m ) return true;
+        return false;
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+
+        int n = bloomDay.length;
+        if(k*m > n) return -1;
+
+        int start = 1;
+        int end = Arrays.stream(bloomDay).max().getAsInt();
+        int answer = -1; // return the minimum number of days
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            //System.out.println(mid);
+            if(boom(bloomDay, m, k, mid)){
+                answer = mid;
+                end = mid-1;
+                
+            }
+            else {
+                start = mid+1;
+            }
+
         }
 
-        return minDays;
+        return answer;
+
     }
 }
